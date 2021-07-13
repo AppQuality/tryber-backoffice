f3291a25047739bdc859884997b37d77d36dbb41
Davide Bizzi <davide.bizzi@app-quality.com>
2021-07-13 18:17:39 +0200

    feat: Allow saving popups

---
+++ src/api/createPopup.js
@@ -0,0 +1,20 @@
+export default  ({ content }, token = false) => {
+  if (process.env.REACT_APP_DEFAULT_TOKEN)
+    token = process.env.REACT_APP_DEFAULT_TOKEN;
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if (token) myHeaders.append("Authorization", `Bearer ${token}`);
+
+  var raw = JSON.stringify({ content }, (token = false));
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw
+  };
+
+  return fetch(
+    process.env.REACT_APP_API_URL + "/popups/",
+    requestOptions
+  ).then(response => response.json());
+};
