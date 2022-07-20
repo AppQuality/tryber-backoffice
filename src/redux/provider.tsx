4e49ff95fd7da48db314245c932f0d7c74b031ff
Iacopo Leardini <iacopo.lea@gmail.com>
2022-07-20 17:47:15 +0200

    feat: integrate redux toolkit in repo

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,21 +1,3 @@
-import { Provider, useDispatch } from 'react-redux';
-import { applyMiddleware, createStore, Store } from 'redux';
-import thunk from 'redux-thunk';
+import { useDispatch } from "react-redux";
 
-import reducer from './reducer';
-
-const middlewares = [thunk];
-const store: Store<
-  GeneralState,
-  {
-    type: string;
-  }
-> & {
-  dispatch: DispatchType;
-} = createStore(reducer, applyMiddleware(...middlewares));
-
-export const useAppDispatch = () => useDispatch<AppDispatch>(); // Export
-
-export default ({ children }: any) => (
-  <Provider store={store}>{children}</Provider>
-);
+export const useAppDispatch = () => useDispatch<AppDispatch>();
