22770a9770865fc48a636576a4871a3643d478d4
alexSp84 <alessandro.spinelli84@outlook.it>
2022-07-21 17:37:50 +0200

    wip(jotform): cuf configurator

--- src/pages/Jotform/CufConfigurator/CufConfigurator.tsx
+++ src/pages/Jotform/CufConfigurator/CufConfigurator.tsx
@@ -1,10 +1,48 @@
-import { Formik } from "@appquality/appquality-design-system";
+import { Button, Form, Formik } from "@appquality/appquality-design-system";
+import { useAppSelector } from "src/store";
 import { CufConfiguratorCard } from "./CufConfiguratorCard";
+import { FormikProps } from "formik";
+
+const initialJotformValues: JotformValues = {
+  additional: {},
+};
 
 export const CufConfigurator = () => {
+  const { fields } = useAppSelector((state) => state.jotform);
+
+  fields.forEach((f) => {
+    if (f.checked)
+      initialJotformValues.additional[f.fieldData.id] = {
+        id: f.fieldData.id,
+        type: f.fieldData.type,
+        question: "",
+        ...(f.fieldData.options ? { options: [] } : undefined),
+      };
+  });
+
   return (
-    <Formik initialValues={{}} onSubmit={async (values, helpers) => {}}>
-      <CufConfiguratorCard />
+    <Formik
+      initialValues={initialJotformValues}
+      onSubmit={async (values, helpers) => {
+        console.log(values);
+      }}
+    >
+      {(formikProps: FormikProps<JotformValues>) => {
+        return (
+          <Form id="jotform">
+            <CufConfiguratorCard />
+            <Button
+              className="aq-mt-1 aq-mb-2"
+              type="primary"
+              htmlType="submit"
+              size="block"
+              flat
+            >
+              Submit
+            </Button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
--- src/pages/Jotform/CufConfigurator/CufConfiguratorCard.tsx
+++ src/pages/Jotform/CufConfigurator/CufConfiguratorCard.tsx
@@ -1,12 +1,43 @@
 import {
   Card,
-  Checkbox,
   Field,
+  Select,
   Text,
 } from "@appquality/appquality-design-system";
+import { useAppSelector } from "src/store";
 
 export const CufConfiguratorCard = () => {
+  const { fields } = useAppSelector((state) => state.jotform);
+
   return (
-    <Card title={"Additional fields configurator"} className="aq-mb-3"></Card>
+    <Card title={"Additional fields configurator"} className="aq-mb-3">
+      {fields?.map(
+        (f) =>
+          f.checked && (
+            <div key={`configurator-${f.fieldData.id}`}>
+              <Text className="aq-mb-2">
+                <strong>{f.fieldData.name.it}</strong>
+              </Text>
+              <Field
+                type="text"
+                name={`additional.${f.fieldData.id}.question`}
+                label="Question"
+                placeholder="Write a question"
+              />
+              {f.fieldData.options && (
+                <Select
+                  name={`additional.${f.fieldData.id}.options`}
+                  value={{ value: "", label: "" }}
+                  label={"Options"}
+                  options={f.fieldData.options.map((o) => {
+                    return { value: o.id.toString(), label: o.name };
+                  })}
+                  isMulti
+                />
+              )}
+            </div>
+          )
+      )}
+    </Card>
   );
 };
