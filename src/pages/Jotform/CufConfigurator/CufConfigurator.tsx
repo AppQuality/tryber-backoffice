381d7fe4f1e7d44c4e370e13261585fee591650c
Iacopo Leardini <iacopo.lea@gmail.com>
2022-07-25 15:57:10 +0200

    fix(jotform): put cuf initial values in component state

--- src/pages/Jotform/CufConfigurator/CufConfigurator.tsx
+++ src/pages/Jotform/CufConfigurator/CufConfigurator.tsx
@@ -1,29 +1,30 @@
 import { Button, Form, Formik } from "@appquality/appquality-design-system";
 import { useAppSelector } from "src/store";
 import { CufConfiguratorCard } from "./CufConfiguratorCard";
-import { FormikProps } from "formik";
 import * as yup from "yup";
 import FocusError from "./FocusError";
 import { FormTitleCard } from "./FormTitleCard";
-import { useEffect } from "react";
-
-const initialJotformValues: JotformValues = {
-  formTitle: "",
-  additional: {},
-};
+import { useEffect, useState } from "react";
 
 export const CufConfigurator = () => {
+  const [initialCufValues, setInitialCufValues] = useState<{
+    [key: string]: any;
+  }>({});
+
   const { list } = useAppSelector((state) => state.jotform);
 
   useEffect(() => {
+    const newValues = {};
     list.forEach((l, i) => {
-      initialJotformValues.additional[i] = {
+      // @ts-ignore
+      newValues[i] = {
         cufId: l.id,
         title: "",
         type: l.type,
         ...(l.options ? { options: [] } : undefined),
       };
     });
+    setInitialCufValues(newValues);
   }, [list]);
 
   const validationSchema = {
@@ -32,7 +33,10 @@ export const CufConfigurator = () => {
 
   return (
     <Formik
-      initialValues={initialJotformValues}
+      initialValues={{
+        formTitle: "",
+        additional: initialCufValues,
+      }}
       validationSchema={yup.object(validationSchema)}
       onSubmit={async (values, helpers) => {
         let toBeSendValues: FormElement[] = [];
@@ -55,25 +59,21 @@ export const CufConfigurator = () => {
         console.log(toBeSendValues);
       }}
     >
-      {(formikProps: FormikProps<JotformValues>) => {
-        return (
-          <Form id="jotform">
-            <FormTitleCard />
-            <CufConfiguratorCard />
-            <Button
-              className="aq-mt-1 aq-mb-4"
-              type="primary"
-              htmlType="submit"
-              size="block"
-              flat
-              disabled={!list.length}
-            >
-              Submit
-            </Button>
-            <FocusError />
-          </Form>
-        );
-      }}
+      <Form id="jotform">
+        <FormTitleCard />
+        <CufConfiguratorCard />
+        <Button
+          className="aq-mt-1 aq-mb-4"
+          type="primary"
+          htmlType="submit"
+          size="block"
+          flat
+          disabled={!list.length}
+        >
+          Submit
+        </Button>
+        <FocusError />
+      </Form>
     </Formik>
   );
 };
