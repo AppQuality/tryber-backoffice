c826849e3817beced453ea954049718bfedab228
Iacopo Leardini <iacopo.lea@gmail.com>
2022-08-12 16:17:38 +0200

    feat(jotform): tester and subscriber role cannot view the page

--- src/pages/Jotform/index.tsx
+++ src/pages/Jotform/index.tsx
@@ -4,6 +4,7 @@ import { CufConfigurator } from "./CufConfigurator/CufConfigurator";
 import { CufListCard } from "./CufListCard";
 import { useAppSelector } from "src/store";
 import { JotformSuccessCard } from "src/pages/Jotform/JotformSuccessCard";
+import useUserData from "src/pages/Jotform/useUserData";
 
 const StickyContainer = styled.div`
   @media (min-width: ${(p) => p.theme.grid.breakpoints.lg}) {
@@ -13,7 +14,16 @@ const StickyContainer = styled.div`
 `;
 
 export default function Jotform() {
+  const { isFetching, isError, data, isLoading } = useUserData();
   const { url } = useAppSelector((state) => state.jotform);
+  if (isLoading || isFetching) return <Container>loading...</Container>;
+  if (isError) return <Container>there was an error</Container>;
+  if (data?.role === "tester" || data?.role === "subscriber")
+    return (
+      <Container>
+        You can't see this page because you are not authorized
+      </Container>
+    );
   return (
     <Container>
       <BSGrid className="aq-mt-4">
