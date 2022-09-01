1c2d3911d9f1ffa4c73e7a89dc23610ac8177684
Iacopo Leardini <iacopo.lea@gmail.com>
2022-09-01 17:20:11 +0200

    feat: init form configurator

--- src/api/getOnePopup.js
+++ src/api/getOnePopup.js
@@ -3,9 +3,9 @@ import getAllPopups from "./getAllPopups";
 export default (id, token = false) => {
   return new Promise((resolve, reject) =>
     getAllPopups(token)
-      .then(data => {
+      .then((data) => {
         if (!data.length) return reject(Error("No data"));
-        const res = data.filter(p => p.id == id);
+        const res = data.filter((p) => p.id === id);
         if (!res.length) return reject(Error("No data"));
         resolve(res[0]);
       })
