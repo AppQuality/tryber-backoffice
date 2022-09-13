1f307fefa6dc0ac0bd6ee112e39aa4b0c0141c07
alexSp84 <alessandro.spinelli84@outlook.it>
2022-09-13 16:34:50 +0200

    feat: preselection form list with live search

--- src/pages/campaigns/preselectionFormList/formTableCard/index.tsx
+++ src/pages/campaigns/preselectionFormList/formTableCard/index.tsx
@@ -1,5 +1,85 @@
-import { Card } from "@appquality/appquality-design-system";
+import {
+  Card,
+  Pagination,
+  Table,
+  TableType,
+} from "@appquality/appquality-design-system";
+import { useEffect, useState } from "react";
+import { PencilSquare } from "react-bootstrap-icons";
+import { useHistory } from "react-router-dom";
+import { useGetCampaignsFormsQuery } from "src/services/tryberApi";
+import { useAppDispatch, useAppSelector } from "src/store";
+import { resetList } from "../preselectionListSlice";
+import Columns from "./columns";
+import { FormSearchCard } from "./formSearchCard";
 
 export const FormTableCard = () => {
-  return <Card title="Lista Preselection Form"></Card>;
+  const { search, searchBy } = useAppSelector(
+    (state) => state.campaignPreselectionList
+  );
+  const history = useHistory();
+  const dispatch = useAppDispatch();
+  const [page, setPage] = useState<number>(1);
+  const [rows, setRows] = useState<TableType.Row[]>([]);
+  const columns = Columns();
+  const limit = 10;
+  const { data, isFetching, isLoading } = useGetCampaignsFormsQuery({
+    start: (page - 1) * limit,
+    limit: limit,
+    search,
+    ...(search ? { searchBy: searchBy.join(",") } : {}),
+  });
+
+  useEffect(() => {
+    const a = data?.results?.map((res) => {
+      return {
+        key: res.id,
+        id: res.id,
+        name: res.name,
+        campaignId: res.campaign || (
+          <div className="aq-text-danger">No associated campaigns</div>
+        ),
+        actions: (
+          <PencilSquare
+            size={"18"}
+            className={"aq-text-primaryVariant"}
+            title="Edit"
+            onClick={() =>
+              history.push(`/backoffice/campaigns/preselection/${res.id}`)
+            }
+            style={{ cursor: "pointer" }}
+          />
+        ),
+      };
+    });
+    setRows(a || []);
+  }, [data?.results]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetList());
+    };
+  }, []);
+
+  return (
+    <Card title="Preselection Form List">
+      <FormSearchCard />
+      <Table
+        dataSource={rows}
+        isLoading={isLoading || isFetching}
+        isStriped
+        i18n={{
+          loading: "Loading Data",
+          empty: "No data",
+        }}
+        columns={columns}
+      />
+      <Pagination
+        className="aq-pt-3"
+        onPageChange={setPage}
+        current={page}
+        maxPages={Math.ceil((data?.total || 0) / limit)}
+      />
+    </Card>
+  );
 };
---
+++ src/pages/campaigns/preselectionFormList/preselectionListSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface PreselectionListState {
+  search?: string;
+  searchBy: SearchByType[];
+}
+
+const initialState: PreselectionListState = {
+  searchBy: ["id", "name", "campaignId"],
+};
+
+const campaignPreselectionListSlice = createSlice({
+  name: "campaignPreselectionList",
+  initialState: initialState,
+  reducers: {
+    setSearch(state, action: PayloadAction<string | undefined>) {
+      state.search = action.payload;
+    },
+    setSearchBy(state, action: PayloadAction<SearchByType[]>) {
+      state.searchBy = action.payload;
+    },
+    resetList() {
+      return initialState;
+    },
+  },
+});
+
+const { actions, reducer } = campaignPreselectionListSlice;
+export const { setSearch, setSearchBy, resetList } = actions;
+export default reducer;
