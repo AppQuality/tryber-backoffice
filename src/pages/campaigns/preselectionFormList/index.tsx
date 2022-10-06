098d68008d9f1dfd645b0e035919b3806711ad9a
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-06 16:56:38 +0200

    feat(preselection): change routes

--- src/pages/campaigns/preselectionFormList/index.tsx
+++ src/pages/campaigns/preselectionFormList/index.tsx
@@ -1,10 +1,19 @@
-import { BSCol, BSGrid } from "@appquality/appquality-design-system";
+import { BSCol, BSGrid, Button } from "@appquality/appquality-design-system";
 import { OpsUserContainer } from "../../../features/AuthorizedOnlyContainer";
 import { FormTableCard } from "./formTableCard";
+import { PageTitle } from "@appquality/appquality-design-system";
 
 const PreselectionFormList = () => {
   return (
     <OpsUserContainer>
+      <PageTitle size="regular">
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <span>Preselection Form List</span>
+          <Button as="a" href="preselection-forms/new/" className="aq-ml-4">
+            Add New
+          </Button>
+        </div>
+      </PageTitle>
       <BSGrid className="aq-my-4">
         <BSCol size="col-lg-12" className="aq-mb-4">
           <FormTableCard />
