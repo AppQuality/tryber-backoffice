1b03f0ffc70d84a65a1fabdee0047e0ad8582c46
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-13 11:58:56 +0200

    feat(selection): add empty table

--- src/pages/campaigns/selection/SelectionTable/index.tsx
+++ src/pages/campaigns/selection/SelectionTable/index.tsx
@@ -1,5 +1,12 @@
+import { Table } from "@appquality/appquality-design-system";
+import { columns } from "src/pages/campaigns/selection/SelectionTable/columns";
+
 const SelectionTable = () => {
-  return <div>selection table</div>;
+  return (
+    <>
+      <Table data-testid="selection-table" dataSource={[]} columns={columns} />
+    </>
+  );
 };
 
 export default SelectionTable;
