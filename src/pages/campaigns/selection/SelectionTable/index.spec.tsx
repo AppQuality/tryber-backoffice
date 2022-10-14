e83b7c1fa8425957f68ad63e59a600f6edba2143
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-14 15:14:22 +0200

    feat(selection): setup test & examples and first rows

--- src/pages/campaigns/selection/SelectionTable/index.spec.tsx
+++ src/pages/campaigns/selection/SelectionTable/index.spec.tsx
@@ -1,44 +1,28 @@
-import { renderWithProviders } from "src/utils/test-utils";
+import { renderWithProviders, setupMatchMediaMock } from "src/utils/test-utils";
 import SelectionTable from "src/pages/campaigns/selection/SelectionTable/index";
 import { findByTestId, findByText } from "@testing-library/react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
+import { getCandidatesMock } from "src/pages/campaigns/selection/SelectionTable/_mocked_data_";
 
 const handlers = [
-  rest.get("http://localhost/campaigns/1/candidates", (req, res, ctx) => {
-    return res(
-      ctx.json({
-        results: [
-          {
-            id: 123,
-            name: "Pippo",
-            surname: "Franco",
-            experience: 200,
-            level: "Bronze",
-            devices: [
-              {
-                manufacturer: "Apple",
-                model: "iPhone",
-                os: "iOS",
-                osVersion: "9",
-              },
-              {
-                os: "Windows",
-                osVersion: "Windows 10 May 2021 Update",
-              },
-            ],
-          },
-        ],
-        start: 0,
-        size: 1,
-      }),
-      ctx.delay(50)
-    );
-  }),
+  rest.get(
+    "https://dv-crowd.app-quality.com/api/campaigns/1/candidates",
+    async (req, res, ctx) => {
+      return res(ctx.json({ results: [] }), ctx.delay(50));
+    }
+  ),
+  rest.get(
+    "https://dv-crowd.app-quality.com/api/campaigns/2/candidates",
+    async (req, res, ctx) => {
+      const mock = await getCandidatesMock("example-2");
+      return res(ctx.json(mock), ctx.delay(50));
+    }
+  ),
 ];
 
 const server = setupServer(...handlers);
-
+setupMatchMediaMock();
 // Enable API mocking before tests.
 beforeAll(() => server.listen());
 
@@ -50,7 +34,7 @@ afterAll(() => server.close());
 
 describe("Selection table", () => {
   it("should render an empty state if no data is provided", async function () {
-    const { findByText } = renderWithProviders(<SelectionTable id="2" />);
+    const { findByText } = renderWithProviders(<SelectionTable id="1" />);
     expect(await findByText("There's no data here")).toBeVisible();
   });
   it("should show the column names", async function () {
@@ -63,13 +47,9 @@ describe("Selection table", () => {
   });
   it("should show the device if data are provided", async function () {
     const { findByText, findByTestId } = renderWithProviders(
-      <SelectionTable id="1" />
+      <SelectionTable id="2" />
     );
-    expect(await findByText("Pippo Franco T123")).toBeVisible();
-    expect(await findByText("Bronze")).toBeVisible();
-    expect(await findByText("200")).toBeVisible();
-    expect(await findByText("iOS 9")).toBeVisible();
-    expect(await findByText("Apple iPhone")).toBeVisible();
-    expect(await findByTestId("device-select-checkbox")).toBeVisible();
+    expect(await findByText("Samsung Galaxy M31")).toBeVisible();
+    // expect(await findByTestId("device-select-checkbox")).toBeVisible();
   });
 });
