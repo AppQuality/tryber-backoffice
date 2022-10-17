cce5d7044e9415478f979351dcbbb39028d5576e
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-17 20:02:21 +0200

    wip(selection): confirm modal action

--- src/pages/campaigns/selection/confirmModal/ConfirmModal.tsx
+++ src/pages/campaigns/selection/confirmModal/ConfirmModal.tsx
@@ -1,16 +1,56 @@
-import { Modal } from "@appquality/appquality-design-system";
+import {
+  BSCol,
+  BSGrid,
+  Button,
+  Modal,
+} from "@appquality/appquality-design-system";
 import { useAppDispatch, useAppSelector } from "src/store";
 import { closeConfirmModal } from "src/pages/campaigns/selection/selectionSlice";
+import { usePostCampaignsByCampaignCandidatesMutation } from "src/services/tryberApi";
 
 const ConfirmModal = () => {
-  const { isConfirmModalOpen } = useAppSelector((state) => state.selection);
+  const { isConfirmModalOpen, selectedDevices } = useAppSelector(
+    (state) => state.selection
+  );
   const dispatch = useAppDispatch();
   const close = () => {
     dispatch(closeConfirmModal());
   };
+  const [selectDevices] = usePostCampaignsByCampaignCandidatesMutation();
+  const confirm = async () => {
+    //await selectDevices()
+  };
+  const Footer = () => {
+    return (
+      <BSGrid>
+        <BSCol>
+          <Button onClick={close} type="primary" flat size="block">
+            Cancella
+          </Button>
+        </BSCol>
+        <BSCol>
+          <Button
+            onClick={confirm}
+            type="primary"
+            flat
+            size="block"
+            disabled={false}
+          >
+            Ok
+          </Button>
+        </BSCol>
+      </BSGrid>
+    );
+  };
   return (
-    <Modal isOpen={isConfirmModalOpen} onClose={close}>
-      pippo
+    <Modal
+      footer={Footer}
+      size="small"
+      isOpen={isConfirmModalOpen}
+      onClose={close}
+    >
+      sono stati selezionati {Object.keys(selectedDevices).length} tester, vuoi
+      procedere?
     </Modal>
   );
 };
