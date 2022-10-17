c4fac31ca9846cdcf78298ea3e8ba273d53ced8e
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-17 18:59:31 +0200

    feat(selection): pagination

--- src/pages/campaigns/selection/selectionSlice.ts
+++ src/pages/campaigns/selection/selectionSlice.ts
@@ -4,10 +4,14 @@ interface SelectionState {
   selectedDevices: {
     [userId: string]: string;
   };
+  currentPage: number;
+  devicesPerPage: number;
 }
 
 const initialState: SelectionState = {
   selectedDevices: {},
+  currentPage: 1,
+  devicesPerPage: 50,
 };
 
 const selectionSlice = createSlice({
@@ -23,9 +27,13 @@ const selectionSlice = createSlice({
     ) {
       state.selectedDevices[action.payload.userId] = action.payload.deviceId;
     },
+    changeTablePage(state, action: PayloadAction<{ newPage: number }>) {
+      if (action.payload.newPage > 0)
+        state.currentPage = action.payload.newPage;
+    },
   },
 });
 
 const { actions, reducer } = selectionSlice;
-export const { reset, checkUserDevice } = actions;
+export const { reset, checkUserDevice, changeTablePage } = actions;
 export default reducer;
