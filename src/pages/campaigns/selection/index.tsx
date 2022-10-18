dbb07350ba671075e754533e5512d6be9feccb48
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-18 18:13:40 +0200

    wip(selection): table unauthorized error

--- src/pages/campaigns/selection/index.tsx
+++ src/pages/campaigns/selection/index.tsx
@@ -11,9 +11,12 @@ import SelectionTable from "./SelectionTable";
 import { useParams } from "react-router-dom";
 import ConfirmButton from "src/pages/campaigns/selection/confirmButton/ConfirmButton";
 import ConfirmModal from "src/pages/campaigns/selection/confirmModal/ConfirmModal";
+import useTableRows from "src/pages/campaigns/selection/SelectionTable/useTableRows";
+import ErrorUnauthorized from "src/pages/campaigns/selection/ErrorUnauthorized/ErrorUnauthorized";
 
 const SelectionPage = () => {
   const { id } = useParams<{ id: string }>();
+  const { error } = useTableRows(id.toString());
   return (
     <div className="selection-page">
       <OpsUserContainer>
@@ -26,13 +29,21 @@ const SelectionPage = () => {
             </Card>
           </BSCol>
           <BSCol size="col-lg-12" className="aq-mt-3">
-            <Card className="aq-mb-3">
-              <Counter />
-              <SelectionTable id={id} />
-            </Card>
-            <Card>
-              <ConfirmButton />
-            </Card>
+            {error && "status" in error && error.status === 502 ? (
+              <Card>
+                <ErrorUnauthorized />
+              </Card>
+            ) : (
+              <>
+                <Card className="aq-mb-3">
+                  <Counter />
+                  <SelectionTable id={id} />
+                </Card>
+                <Card>
+                  <ConfirmButton />
+                </Card>
+              </>
+            )}
           </BSCol>
         </BSGrid>
       </OpsUserContainer>
