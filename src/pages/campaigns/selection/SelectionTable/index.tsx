f337e1fe6e35fa6e2611845572972f18895653fe
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-18 09:49:01 +0200

    wip(selection): selection table error

--- src/pages/campaigns/selection/SelectionTable/index.tsx
+++ src/pages/campaigns/selection/SelectionTable/index.tsx
@@ -1,4 +1,4 @@
-import { Table } from "@appquality/appquality-design-system";
+import { Table, Text } from "@appquality/appquality-design-system";
 import { columns } from "src/pages/campaigns/selection/SelectionTable/columns";
 import { FC } from "react";
 import useTableRows from "src/pages/campaigns/selection/SelectionTable/useTableRows";
@@ -6,6 +6,8 @@ import { Pagination } from "@appquality/appquality-design-system";
 import { useAppSelector, useAppDispatch } from "src/store";
 import { changeTablePage } from "src/pages/campaigns/selection/selectionSlice";
 import styled from "styled-components";
+import tryber from "src/pages/campaigns/selection/SelectionTable/assets/tryber.png";
+import bg from "src/pages/campaigns/selection/SelectionTable/assets/bg.png";
 
 const StyledTable = styled.div`
   .cell.highlighted {
@@ -15,11 +17,35 @@ const StyledTable = styled.div`
 
 const SelectionTable: FC<{ id: string }> = ({ id }) => {
   const dispatch = useAppDispatch();
-  const { rows, totalRows, isFetching } = useTableRows(id);
+  const { rows, totalRows, isFetching, error } = useTableRows(id);
   const { devicesPerPage, currentPage } = useAppSelector(
     (state) => state.selection
   );
 
+  if (error) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexFlow: "column",
+        }}
+      >
+        <img
+          alt="error 403"
+          className="aq-mb-3"
+          src={tryber}
+          style={{ maxWidth: 100 }}
+        />
+        <Text style={{ textAlign: "center" }}>
+          <strong>Non c'è niente da vedere qui</strong>
+          <br />
+          Sembrerebbe che tu non abbia i permessi per accedere a questa pagina
+        </Text>
+      </div>
+    );
+  }
   return (
     <StyledTable>
       <Table
