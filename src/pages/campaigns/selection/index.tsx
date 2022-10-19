b89c0af51177daa9b28f996867c22303e61f3e33
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-19 10:14:46 +0200

    feat(backofice): unauthorized page

--- src/pages/campaigns/selection/index.tsx
+++ src/pages/campaigns/selection/index.tsx
@@ -11,12 +11,9 @@ import SelectionTable from "./SelectionTable";
 import { useParams } from "react-router-dom";
 import ConfirmButton from "src/pages/campaigns/selection/confirmButton/ConfirmButton";
 import ConfirmModal from "src/pages/campaigns/selection/confirmModal/ConfirmModal";
-import useTableRows from "src/pages/campaigns/selection/SelectionTable/useTableRows";
-import ErrorUnauthorized from "src/pages/campaigns/selection/ErrorUnauthorized/ErrorUnauthorized";
 
 const SelectionPage = () => {
   const { id } = useParams<{ id: string }>();
-  const { error } = useTableRows(id.toString());
   return (
     <div className="selection-page">
       <OpsUserContainer>
@@ -29,21 +26,13 @@ const SelectionPage = () => {
             </Card>
           </BSCol>
           <BSCol size="col-lg-12" className="aq-mt-3">
-            {error && "status" in error && error.status === 403 ? (
-              <Card>
-                <ErrorUnauthorized />
-              </Card>
-            ) : (
-              <>
-                <Card className="aq-mb-3">
-                  <Counter />
-                  <SelectionTable id={id} />
-                </Card>
-                <Card>
-                  <ConfirmButton />
-                </Card>
-              </>
-            )}
+            <Card className="aq-mb-3">
+              <Counter />
+              <SelectionTable id={id} />
+            </Card>
+            <Card>
+              <ConfirmButton />
+            </Card>
           </BSCol>
         </BSGrid>
       </OpsUserContainer>
