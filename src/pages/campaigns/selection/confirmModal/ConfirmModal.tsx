5d5134b48be15846c28fd0d22642e754404cafb1
Iacopo Leardini <iacopo.lea@gmail.com>
2022-10-19 18:23:40 +0200

    feat(selection): post selected devices [skip ci]

--- src/pages/campaigns/selection/confirmModal/ConfirmModal.tsx
+++ src/pages/campaigns/selection/confirmModal/ConfirmModal.tsx
@@ -7,8 +7,11 @@ import {
 import { useAppDispatch, useAppSelector } from "src/store";
 import { closeConfirmModal } from "src/pages/campaigns/selection/selectionSlice";
 import { usePostCampaignsByCampaignCandidatesMutation } from "src/services/tryberApi";
+import { FC } from "react";
+import siteWideMessageStore from "src/redux/siteWideMessages";
 
-const ConfirmModal = () => {
+const ConfirmModal: FC<{ id: string }> = ({ id }) => {
+  const { add } = siteWideMessageStore();
   const { isConfirmModalOpen, selectedDevices } = useAppSelector(
     (state) => state.selection
   );
@@ -18,7 +21,37 @@ const ConfirmModal = () => {
   };
   const [selectDevices] = usePostCampaignsByCampaignCandidatesMutation();
   const confirm = async () => {
-    //await selectDevices()
+    const response = await selectDevices({
+      campaign: id,
+      body: Object.keys(selectedDevices).map((testerId) => ({
+        tester_id: parseInt(testerId),
+        device: parseInt(selectedDevices[testerId]),
+      })),
+    });
+    if ("error" in response) {
+      console.error(response.error);
+      let message = "La richiesta non è andata a buon fine";
+      if ("data" in response.error) {
+        // @ts-ignore
+        message += `: ${response.error.data?.message}`;
+      }
+      add({ type: "danger", message, expire: false });
+    }
+    if ("data" in response) {
+      if ("invalidTesters" in response.data) {
+        add({
+          type: "warning",
+          message: `${response.data.invalidTesters?.length} tryber non sono stati aggiunti per un errore`,
+          expire: false,
+        });
+        console.warn(response.data.invalidTesters);
+      }
+      add({
+        type: "success",
+        message: `${response.data.results.length} tryber selezionati con successo`,
+        expire: false,
+      });
+    }
   };
   const Footer = () => {
     return (
@@ -49,7 +82,7 @@ const ConfirmModal = () => {
       isOpen={isConfirmModalOpen}
       onClose={close}
     >
-      sono stati selezionati {Object.keys(selectedDevices).length} tester, vuoi
+      Sono stati selezionati {Object.keys(selectedDevices).length} tester, vuoi
       procedere con la selezione?
     </Modal>
   );
