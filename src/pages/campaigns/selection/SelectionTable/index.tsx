580a5d0b0cdcca4f3b6499db1017ce192c6325f8
alexSp84 <alessandro.spinelli84@outlook.it>
2022-10-19 16:37:43 +0200

    feat(selection): question columns added

--- src/pages/campaigns/selection/SelectionTable/index.tsx
+++ src/pages/campaigns/selection/SelectionTable/index.tsx
@@ -1,24 +1,17 @@
 import { Table, Text } from "@appquality/appquality-design-system";
-import { columns } from "src/pages/campaigns/selection/SelectionTable/columns";
 import { FC } from "react";
 import useTableRows from "src/pages/campaigns/selection/SelectionTable/useTableRows";
 import { Pagination } from "@appquality/appquality-design-system";
 import { useAppSelector, useAppDispatch } from "src/store";
 import { changeTablePage } from "src/pages/campaigns/selection/selectionSlice";
-import styled from "styled-components";
 import tryber from "src/pages/campaigns/selection/SelectionTable/assets/tryber.png";
 import bg from "src/pages/campaigns/selection/SelectionTable/assets/bg.png";
-
-const StyledTable = styled.div`
-  .cell.highlighted {
-    background-color: ${(p) => p.theme.colors.gray100};
-  }
-`;
+import { StyledSelectionTable } from "./_style";
 
 const SelectionTable: FC<{ id: string }> = ({ id }) => {
   const dispatch = useAppDispatch();
   const { rows, totalRows, isFetching, error } = useTableRows(id);
-  const { devicesPerPage, currentPage } = useAppSelector(
+  const { devicesPerPage, currentPage, tableColumns } = useAppSelector(
     (state) => state.selection
   );
 
@@ -47,13 +40,13 @@ const SelectionTable: FC<{ id: string }> = ({ id }) => {
     );
   }
   return (
-    <StyledTable>
+    <StyledSelectionTable>
       <Table
         data-testid="selection-table"
         dataSource={rows}
-        columns={columns}
+        columns={tableColumns}
         isLoading={isFetching}
-        className="aq-mb-3"
+        className="aq-mb-3 table-scrollable"
       />
       <Pagination
         maxPages={Math.ceil(totalRows / devicesPerPage)}
@@ -62,7 +55,7 @@ const SelectionTable: FC<{ id: string }> = ({ id }) => {
           dispatch(changeTablePage({ newPage: page }));
         }}
       />
-    </StyledTable>
+    </StyledSelectionTable>
   );
 };
 
