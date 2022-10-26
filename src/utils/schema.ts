1313ef83c77d2d673db8ff0168ae181a6798ddd8
alexSp84 <alessandro.spinelli84@outlook.it>
2022-10-26 15:12:12 +0200

    chore: generate schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -885,6 +885,10 @@ export interface operations {
         start?: components["parameters"]["start"];
         /** The fields to add to the results */
         fields?: string;
+        /** Key-value Array for item filtering */
+        filterByInclude?: unknown;
+        /** Key-value Array for item filtering */
+        filterByExclude?: unknown;
       };
     };
     responses: {
