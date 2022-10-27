469ca922ca6ff766fe14f34b0912e44d19f41655
alexSp84 <alessandro.spinelli84@outlook.it>
2022-10-27 10:42:53 +0200

    refactor(selection): useFiltersValues

---
+++ src/pages/campaigns/selection/editPanel/selectionFilters/useFiltersValues.ts
@@ -0,0 +1,42 @@
+import { FormikHelpers } from "formik";
+import { useAppDispatch } from "src/store";
+import {
+  changeTablePage,
+  clearSelectedDevice,
+  setDisableApplyFilters,
+  setFilters,
+} from "../../selectionSlice";
+
+export const useFiltersValues = () => {
+  const dispatch = useAppDispatch();
+
+  const addFilter = (filter: { [key: string]: string[] }, row: any) => {
+    if (Object.keys(filter).some((k) => k === row.filterBy.value)) {
+      filter[row.filterBy.value].push(row.search);
+      return filter;
+    } else
+      return {
+        ...filter,
+        ...{ [row.filterBy.value]: [row.search] },
+      };
+  };
+
+  const applyFilters = async (
+    values: SelectionFiltersValues,
+    helpers: FormikHelpers<SelectionFiltersValues>
+  ) => {
+    let filterByInclude: { [key: string]: string[] } = {};
+    let filterByExclude: { [key: string]: string[] } = {};
+    values.filters.row?.forEach((r: any) => {
+      if (r.queryType.value === "filterByInclude")
+        filterByInclude = addFilter(filterByInclude, r);
+      else filterByExclude = addFilter(filterByExclude, r);
+    });
+    dispatch(changeTablePage({ newPage: 1 }));
+    dispatch(setDisableApplyFilters(true));
+    dispatch(clearSelectedDevice());
+    dispatch(setFilters({ filterByInclude, filterByExclude }));
+  };
+
+  return { applyFilters };
+};
