458d7a9faf327b7761ce6f72c212e644bbe2410f
Davide Bizzi <davide.bizzi@app-quality.com>
2023-03-07 15:32:37 +0100

    fix: Use correct colors for severities

--- src/pages/BugsList/Table/Severity.tsx
+++ src/pages/BugsList/Table/Severity.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const StyledSeverity = styled.span<{ severityId: number }>`
-  ${({ severityId: id }) => id === 1 && `color: #800208;`}
-  ${({ severityId: id }) => id === 2 && `color: #C78430;`}
-${({ severityId: id }) => id === 3 && `color: #024780;`}
-${({ severityId: id }) => id === 4 && `color: #02807A;`}
+  ${({ severityId: id }) => id === 4 && `color: #800208;`}
+  ${({ severityId: id }) => id === 3 && `color: #C78430;`}
+${({ severityId: id }) => id === 2 && `color: #024780;`}
+${({ severityId: id }) => id === 1 && `color: #02807A;`}
 font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
   text-transform: capitalize;
 `;
