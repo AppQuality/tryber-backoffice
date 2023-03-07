57f83ae69cade6ec2f7c20302d37e2b8c4c0c47b
Davide Bizzi <davide.bizzi@app-quality.com>
2023-03-07 10:29:02 +0100

    feat: Add search

--- src/pages/BugsList/FilterContext.tsx
+++ src/pages/BugsList/FilterContext.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useState } from "react";
 
 type Filters = {
   severities?: number[];
+  search?: string;
 };
 const Context = createContext<{
   filters: Filters;
