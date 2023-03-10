661694cf0f40d456f52fb82213af9f36c7bc2bf6
Davide Bizzi <davide.bizzi@app-quality.com>
2023-03-10 15:44:30 +0100

    fix: Fix stats

--- src/pages/BugsList/Stats/copyResume.tsx
+++ src/pages/BugsList/Stats/copyResume.tsx
@@ -12,44 +12,44 @@ const useCopyResume = (id: string) => {
   }
   return () => {
     copyToClipboard(`Ciao,
-    al momento sono presenti ${
+al momento sono presenti ${
       data.allBugs.approved
     } bug approvati nella campagna, di cui ${
       data.uniqueBugs.approved
     } sono unici. I bug unici segnalati fino a questo momento sono distribuiti come segue:
 ${data.bugsByType
-  .map((bug) => `    - ${bug.type}: ${bug.value} (${bug.percent}%)`)
+  .map((bug) => `- ${bug.type}: ${bug.value} (${bug.percent}%)`)
   .join("\n")}
-    La severità dei bug finora segnalati è la seguente:
-    - LOW: ${
+La severità dei bug finora segnalati è la seguente:
+- LOW: ${
       "LOW" in data.bugsBySeverity
         ? `${data.bugsBySeverity["LOW"]} (${Math.round(
             (data.bugsBySeverity["LOW"] / data.uniqueBugs.approved) * 100
           )}%)`
         : `0 (0%)`
     }
-    - MEDIUM: ${
+- MEDIUM: ${
       "MEDIUM" in data.bugsBySeverity
         ? `${data.bugsBySeverity["MEDIUM"]} (${Math.round(
             (data.bugsBySeverity["MEDIUM"] / data.uniqueBugs.approved) * 100
           )}%)`
         : `0 (0%)`
     }
-    - HIGH: ${
+- HIGH: ${
       "HIGH" in data.bugsBySeverity
         ? `${data.bugsBySeverity["HIGH"]} (${Math.round(
             (data.bugsBySeverity["HIGH"] / data.uniqueBugs.approved) * 100
           )}%)`
         : `0 (0%)`
     }
-    - CRITICAL: ${
+- CRITICAL: ${
       "CRITICAL" in data.bugsBySeverity
         ? `${data.bugsBySeverity["CRITICAL"]} (${Math.round(
             (data.bugsBySeverity["CRITICAL"] / data.uniqueBugs.approved) * 100
           )}%)`
         : `0 (0%)`
     }
-    Fino a questo momento un totale di ${data.activeTesters} / ${
+Fino a questo momento un totale di ${data.activeTesters} / ${
       data.totalTesters
     } (${
       data.activeTestersPercent
