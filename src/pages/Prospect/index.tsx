3d7df2e2eb83c77b03113229ba488ab65aa7e419
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-03-23 16:19:47 +0100

    feat(page-prospect): add links to old prospect, assistant, add booty

--- src/pages/Prospect/index.tsx
+++ src/pages/Prospect/index.tsx
@@ -43,6 +43,10 @@ type Row = {
   status: string;
 };
 
+const HeaderButton = (props: Parameters<typeof Button>[0]) => {
+  return <Button {...props} size="sm" type="link-hover" />;
+};
+
 const EdiTableWithType = EdiTable<Row>;
 
 const MyEdiTable = styled(EdiTableWithType)`
@@ -179,6 +183,28 @@ const Prospect = () => {
   return (
     <FluidContainer>
       <Title size="xl">Prospect</Title>
+      <HeaderButton
+        as="a"
+        href={`/wp-admin/admin.php?page=cp-prospect&id=${id}`}
+        type="secondary"
+        className="aq-mr-2"
+      >
+        {"<"} Go to old Prospect
+      </HeaderButton>
+      <HeaderButton
+        as="a"
+        href={`/wp-admin/admin.php?page=cp-prospect-crowd&id=${id}`}
+        className="aq-mr-2"
+      >
+        Go to Assistant
+      </HeaderButton>
+      <HeaderButton
+        as="a"
+        href={`/wp-admin/admin.php?page=add-booty&cid=${id}`}
+        className="aq-mr-2"
+      >
+        Go to Add Booty
+      </HeaderButton>
       <Title size="mt" className="aq-mb-3">
         Campaign {id}
       </Title>
