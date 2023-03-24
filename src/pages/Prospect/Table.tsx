5907ee2e52eed637d6f817bc063aab581045db8b
Davide Bizzi <davide.bizzi@app-quality.com>
2023-03-24 11:47:55 +0100

    rework: Split layout from table

--- src/pages/Prospect/Table.tsx
+++ src/pages/Prospect/Table.tsx
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import EdiTable from "./EdiTable";
 import { useEffect, useState } from "react";
 import {
@@ -9,7 +8,6 @@ import {
 } from "src/services/tryberApi";
 import {
   Button,
-  Title,
   aqBootstrapTheme,
   icons,
 } from "@appquality/appquality-design-system";
@@ -21,16 +19,6 @@ import euroRenderer from "./euroRenderer";
 import OpenableColumnButton from "./OpenableColumnButton";
 
 const { TrophyFill } = icons;
-
-const FluidContainer = styled.div`
-    max-width: 90%;
-    margin: 0 auto;
-  }
-`;
-
-const HeaderButton = (props: Parameters<typeof Button>[0]) => {
-  return <Button {...props} size="sm" type="link-hover" />;
-};
 
 const EdiTableWithType = EdiTable<Row>;
 
@@ -184,33 +172,7 @@ const Table = ({ id }: { id: string }) => {
   const isDone = data.items.some((p) => p.status === "done");
 
   return (
-    <FluidContainer>
-      <Title size="xl">Prospect</Title>
-      <HeaderButton
-        as="a"
-        href={`/wp-admin/admin.php?page=cp-prospect&id=${id}`}
-        type="secondary"
-        className="aq-mr-2"
-      >
-        {"<"} Go to old Prospect
-      </HeaderButton>
-      <HeaderButton
-        as="a"
-        href={`/wp-admin/admin.php?page=cp-prospect-crowd&id=${id}`}
-        className="aq-mr-2"
-      >
-        Go to Assistant
-      </HeaderButton>
-      <HeaderButton
-        as="a"
-        href={`/wp-admin/admin.php?page=add-booty&cid=${id}`}
-        className="aq-mr-2"
-      >
-        Go to Add Booty
-      </HeaderButton>
-      <Title size="mt" className="aq-mb-3">
-        Campaign {id}
-      </Title>
+    <>
       <Button
         type="primary"
         onClick={() => {
@@ -611,7 +573,7 @@ const Table = ({ id }: { id: string }) => {
         ]}
       />
       MAX BONUS BUG: {payouts.maxBonusBug}
-    </FluidContainer>
+    </>
   );
 };
 
