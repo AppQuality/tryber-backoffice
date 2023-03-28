f3329817140c996915f9014943502d726cdfaa64
Davide Bizzi <davide.bizzi@app-quality.com>
2023-03-28 11:56:26 +0200

    fix: Use old bug emoji

--- src/pages/Prospect/Table/cellRenderer.tsx
+++ src/pages/Prospect/Table/cellRenderer.tsx
@@ -3,5 +3,5 @@ import { Row } from "../types";
 
 export const euroRenderer = (value: Item<Row>[keyof Row]) => `${value} €`;
 export const bugRenderer = (value: Item<Row>[keyof Row]) =>
-  value.toString().length > 0 ? `${value} 🪲` : "";
+  value.toString().length > 0 ? `${value} 🐛` : "";
 export const pointsRenderer = (value: Item<Row>[keyof Row]) => `${value} pts.`;
