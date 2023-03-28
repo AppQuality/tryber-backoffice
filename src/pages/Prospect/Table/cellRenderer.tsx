e4fa7a997c4203121444e8bc1cf5123336a617db
Davide Bizzi <davide.bizzi@app-quality.com>
2023-03-28 11:31:12 +0200

    feat: Use renderer on topheaders

--- src/pages/Prospect/Table/cellRenderer.tsx
+++ src/pages/Prospect/Table/cellRenderer.tsx
@@ -2,5 +2,6 @@ import { Item } from "../EdiTable/types";
 import { Row } from "../types";
 
 export const euroRenderer = (value: Item<Row>[keyof Row]) => `${value} €`;
-export const bugRenderer = (value: Item<Row>[keyof Row]) => `${value} 🪲`;
+export const bugRenderer = (value: Item<Row>[keyof Row]) =>
+  value.toString().length > 0 ? `${value} 🪲` : "";
 export const pointsRenderer = (value: Item<Row>[keyof Row]) => `${value} pts.`;
