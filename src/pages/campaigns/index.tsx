5fdb119caf85fb4fb6786c3c95c2c5172b33e2e9
Davide Bizzi <davide.bizzi@unguess.io>
2023-05-04 12:44:57 +0200

    feat: Add navigation via post to exit iframe

--- src/pages/campaigns/index.tsx
+++ src/pages/campaigns/index.tsx
@@ -1,14 +1,11 @@
-import FakeWPwrapper from "src/components/FakeWpWrapper";
 import CampaignsTable from "./components/table";
 import FiltersContext from "./components/table/FilterContext";
 
 const Campaigns = () => {
   return (
-    <FakeWPwrapper>
-      <FiltersContext>
-        <CampaignsTable />
-      </FiltersContext>
-    </FakeWPwrapper>
+    <FiltersContext>
+      <CampaignsTable />
+    </FiltersContext>
   );
 };
 export default Campaigns;
