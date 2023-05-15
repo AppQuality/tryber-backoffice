d026f5ea1b3a7d81fa36a622bbc44c7cf100b0e0
Davide Bizzi <davide.bizzi@unguess.io>
2023-05-15 14:56:46 +0200

    feat: Handle filter by csm

--- src/pages/campaigns/components/table/Filters/MyCampaign.tsx
+++ src/pages/campaigns/components/table/Filters/MyCampaign.tsx
@@ -1,22 +1,45 @@
 import { Select } from "@appquality/appquality-design-system";
+import {
+  useGetCampaignsOwnersQuery,
+  useGetUsersMeQuery,
+} from "src/services/tryberApi";
 import { useFiltersCardContext } from "../FilterContext";
 
 const MyCampaign = () => {
+  const { data, isLoading } = useGetUsersMeQuery({ fields: "id" });
+  const { data: owners, isLoading: ownersLoading } =
+    useGetCampaignsOwnersQuery();
   const { setFilters, filters } = useFiltersCardContext();
+  if (isLoading || !data || ownersLoading || !owners) return null;
+
+  const myId = data.id;
+  const options = owners.map((owner) =>
+    owner.id === myId
+      ? {
+          value: "mine",
+          label: "Managed by me",
+        }
+      : {
+          value: owner.id.toString(),
+          label: `${owner.name.charAt(0).toUpperCase()}. ${owner.surname}`,
+        }
+  );
+
+  let currentValue = "all";
+  if (filters?.csm === myId) currentValue = "mine";
+  else if (filters?.csm) currentValue = filters.csm.toString();
   return (
     <Select
       name="mine"
       label=""
       isClearable={false}
-      options={[
-        { value: "mine", label: "Managed by me" },
-        { value: "all", label: "Managed by anyone" },
-      ]}
+      options={[...options, { value: "all", label: "Managed by anyone" }]}
       onChange={(item) => {
-        if (item.value === "mine") setFilters({ mine: true });
-        else setFilters({ mine: false });
+        if (item.value === "mine") setFilters({ csm: myId });
+        else if (item.value === "all") setFilters({ csm: undefined });
+        else setFilters({ csm: Number(item.value) });
       }}
-      value={{ value: filters?.mine ? "mine" : "all", label: "" }}
+      value={{ value: currentValue, label: "" }}
     />
   );
 };
