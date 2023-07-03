f17bea830cfb152728e0600406c3b3d14eaf03e4
Luca Cannarozzo <cannarozzoluca@live.it>
2023-07-03 11:59:53 +0200

    feat(agreements): add empty page for agreement listing

---
+++ src/pages/agreements/index.tsx
@@ -0,0 +1,9 @@
+const Agreements = () => {
+  return (
+    <div style={{ background: "#e5e6e6", float: "left", width: "100%" }}>
+      Work in progress
+    </div>
+  );
+};
+
+export default Agreements;
