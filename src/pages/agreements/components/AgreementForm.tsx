3d64786da38f3ae6b56fc3f1c71a899e2c9952b7
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-05 18:26:17 +0200

    wip(agreements): add form cleanup and update test

--- src/pages/agreements/components/AgreementForm.tsx
+++ src/pages/agreements/components/AgreementForm.tsx
@@ -1,13 +1,16 @@
 import {
   Formik,
   Form,
-  FormikField,
   Field,
+  FormikField,
   FormLabel,
+  Select,
   TextareaField,
-  Card,
   Button,
   Checkbox,
+  FormGroup,
+  FieldProps,
+  ErrorMessage,
 } from "@appquality/appquality-design-system";
 import * as yup from "yup";
 
@@ -57,25 +60,44 @@ const AgreementForm = () => {
         onSubmit={onSubmit}
       >
         <Form>
-          <Card>
-            <FormLabel htmlFor="title" label="Title" />
-            <Field name="title" />
-            <FormLabel htmlFor="tokens" label="Tokens" />
-            <Field name="tokens" />
-            <FormLabel htmlFor="tokenUnitPrice" label="Token Unit Price" />
-            <Field name="tokenUnitPrice" />
-            <FormLabel htmlFor="startDate" label="Start Date" />
-            <Field name="startDate" />
-            <FormLabel htmlFor="closeDate" label="Close Date" />
-            <Field name="closeDate" />
-            <FormLabel htmlFor="isTokenBased" label="Is Token Based" />
-            <Checkbox id="isTokenBased" name="isTokenBased" />
-            <FormLabel htmlFor="customer" label="Customer" />
-            <Field name="customer" />
-            <FormLabel htmlFor="notes" label="Notes" />
-            <TextareaField name="notes" placeholder="Notes" />
-            <Button htmlType="submit">Submit</Button>
-          </Card>
+          <FormLabel htmlFor="title" label="Title" />
+          <Field name="title" />
+          <FormLabel htmlFor="tokens" label="Tokens" />
+          <Field name="tokens" />
+          <FormLabel htmlFor="tokenUnitPrice" label="Token Unit Price" />
+          <Field name="tokenUnitPrice" />
+          <FormLabel htmlFor="startDate" label="Start Date" />
+          <Field name="startDate" />
+          <FormLabel htmlFor="closeDate" label="Close Date" />
+          <Field name="closeDate" />
+          <FormLabel htmlFor="isTokenBased" label="Is Token Based" />
+          <Checkbox id="isTokenBased" name="isTokenBased" />
+          <FormikField name="customer">
+            {({ field, form }: FieldProps) => {
+              return (
+                <FormGroup>
+                  <Select
+                    data-qa="customer-select"
+                    isMulti
+                    name={field.name}
+                    onBlur={() => {
+                      form.setFieldTouched(field.name);
+                    }}
+                    onChange={(value) => {
+                      form.setFieldValue(field.name, value, true);
+                    }}
+                    label="Customer"
+                    value={field.value}
+                    options={[]}
+                  />
+                  <ErrorMessage name={field.name} />
+                </FormGroup>
+              );
+            }}
+          </FormikField>
+          <FormLabel htmlFor="notes" label="Notes" />
+          <TextareaField name="notes" placeholder="Notes" />
+          <Button htmlType="submit">Submit</Button>
         </Form>
       </Formik>
     </div>
