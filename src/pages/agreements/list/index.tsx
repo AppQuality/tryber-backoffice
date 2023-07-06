620e7f820761492097d0d97923c5ebfa2894ea06
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-06 18:32:07 +0200

    wip(agreements): fake list page navigation

--- src/pages/agreements/list/index.tsx
+++ src/pages/agreements/list/index.tsx
@@ -1,5 +1,23 @@
+import { Button, PageTitle } from "@appquality/appquality-design-system";
+import { useHistory } from "react-router-dom";
+import { OpsUserContainer } from "src/features/AuthorizedOnlyContainer";
+
 const AgreementsListPage = () => {
-  return <div>Work in progress</div>;
+  const history = useHistory();
+  return (
+    <OpsUserContainer>
+      <PageTitle>Agreements</PageTitle>
+      <div className="aq-mb-3">work in progress</div>
+      <Button
+        type="primary"
+        onClick={() => {
+          history.push("/backoffice/agreements/new");
+        }}
+      >
+        create new agreement
+      </Button>
+    </OpsUserContainer>
+  );
 };
 
 export default AgreementsListPage;
