59bd201b61adc93de3a8515e9eee0da719149cde
Luca Cannarozzo <cannarozzoluca@live.it>
2023-07-06 19:40:04 +0200

    fix(agreements-list): wrap page in OpsContainer

--- src/pages/agreements/list/index.tsx
+++ src/pages/agreements/list/index.tsx
@@ -2,11 +2,13 @@ import {
   BSCol,
   BSGrid,
   Button,
+  Card,
   Title,
 } from "@appquality/appquality-design-system";
 import FilterContext from "src/pages/agreements/list/FilterContext";
 import styled from "styled-components";
 import CustomersFilter from "src/pages/agreements/list/Filters/CustomersFilter";
+import { OpsUserContainer } from "src/features/AuthorizedOnlyContainer";
 import { Agreements } from "./Agreements";
 
 const FluidContainer = styled.div`
@@ -26,33 +28,37 @@ const HeaderButton = (props: Parameters<typeof Button>[0]) => {
 
 const AgreementsListPage = () => {
   return (
-    <FluidContainer>
-      <FlexContainer>
-        <Title size="m">Agreements List</Title>
-        <HeaderButton
-          as="a"
-          href={`/backoffice/agreements/new`}
-          className="aq-mr-2"
-        >
-          + Add New
-        </HeaderButton>
-      </FlexContainer>
-      <FilterContext>
-        <div
-          className="aq-mb-4"
-          style={{
-            margin: "1rem 0",
-          }}
-        >
-          <CustomersFilter />
-        </div>
-        <BSGrid>
-          <BSCol size="col-lg-12">
-            <Agreements />
-          </BSCol>
-        </BSGrid>
-      </FilterContext>
-    </FluidContainer>
+    <OpsUserContainer>
+      <FluidContainer>
+        <FlexContainer>
+          <Title size="m">Agreements List</Title>
+          <HeaderButton
+            as="a"
+            href={`/backoffice/agreements/new`}
+            className="aq-mr-2"
+          >
+            + Add New
+          </HeaderButton>
+        </FlexContainer>
+        <Card className="aq-pb-4">
+          <FilterContext>
+            <div
+              className="aq-mb-4"
+              style={{
+                margin: "1rem 0",
+              }}
+            >
+              <CustomersFilter />
+            </div>
+            <BSGrid>
+              <BSCol size="col-lg-12">
+                <Agreements />
+              </BSCol>
+            </BSGrid>
+          </FilterContext>
+        </Card>
+      </FluidContainer>
+    </OpsUserContainer>
   );
 };
 
