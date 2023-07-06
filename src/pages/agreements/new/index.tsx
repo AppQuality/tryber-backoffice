ab794ccbe08265710bd7dc50e491e9dc0aca1f1b
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-06 18:24:03 +0200

    refactor(agreement): separate onsubmit functions

--- src/pages/agreements/new/index.tsx
+++ src/pages/agreements/new/index.tsx
@@ -1,8 +1,46 @@
 import { OpsUserContainer } from "src/features/AuthorizedOnlyContainer";
-import { AgreementForm } from "../components/AgreementForm";
+import {
+  AgreementForm,
+  AgreementFormValues,
+} from "../components/AgreementForm";
 import { Card, PageTitle } from "@appquality/appquality-design-system";
+import { usePostAgreementsMutation } from "src/services/tryberApi";
+import { FormikHelpers } from "formik";
+import { useHistory } from "react-router-dom";
+import siteWideMessageStore from "src/redux/siteWideMessages";
 
 const NewAgreementPage = () => {
+  const [newAgreement] = usePostAgreementsMutation();
+  const history = useHistory();
+  const { add } = siteWideMessageStore();
+  const onSubmit = async (
+    values: AgreementFormValues,
+    actions: FormikHelpers<AgreementFormValues>
+  ) => {
+    actions.setSubmitting(true);
+    const res = await newAgreement({
+      body: {
+        title: values.title,
+        tokens: values.tokens,
+        unitPrice: values.tokenUnitPrice,
+        startDate: values.startDate,
+        expirationDate: values.expirationDate,
+        isTokenBased: values.isTokenBased,
+        note: values.note,
+        customerId: parseInt(values.customer),
+      },
+    });
+    if (res && "data" in res) {
+      history.push(`/backoffice/agreements/${res.data.agreementId}`);
+      add({ type: "success", message: "New Agreement Saved" });
+      return;
+    }
+    add({
+      type: "danger",
+      message: "There was an error",
+    });
+    return;
+  };
   return (
     <OpsUserContainer>
       <PageTitle
@@ -11,7 +49,7 @@ const NewAgreementPage = () => {
         New Agreement
       </PageTitle>
       <Card className="aq-pb-4">
-        <AgreementForm />
+        <AgreementForm onSubmit={onSubmit} />
       </Card>
     </OpsUserContainer>
   );
--- src/pages/agreements/view-edit/index.tsx
+++ src/pages/agreements/view-edit/index.tsx
@@ -1,11 +1,22 @@
 import { Card, PageTitle } from "@appquality/appquality-design-system";
 import { OpsUserContainer } from "src/features/AuthorizedOnlyContainer";
-import { AgreementForm } from "../components/AgreementForm";
-import { useGetAgreementsByAgreementIdQuery } from "src/services/tryberApi";
-import { useParams } from "react-router-dom";
+import { FormikHelpers } from "formik";
+import {
+  AgreementForm,
+  AgreementFormValues,
+} from "../components/AgreementForm";
+import {
+  useGetAgreementsByAgreementIdQuery,
+  usePutAgreementsByAgreementIdMutation,
+} from "src/services/tryberApi";
+import { useHistory, useParams } from "react-router-dom";
+import siteWideMessageStore from "src/redux/siteWideMessages";
 
 const EditAgreementPage = () => {
   const { id } = useParams<{ id: string }>();
+  const [editAgreement] = usePutAgreementsByAgreementIdMutation();
+  const history = useHistory();
+  const { add } = siteWideMessageStore();
   // todo: understand better which data is being fetched here
   const { data, currentData, isLoading, isFetching, isError, refetch } =
     useGetAgreementsByAgreementIdQuery({
@@ -13,6 +24,39 @@ const EditAgreementPage = () => {
     });
   if (isLoading || isFetching) return <div>loading...</div>;
   if (isError) return <div>there was an error</div>;
+
+  const onSubmit = async (
+    values: AgreementFormValues,
+    actions: FormikHelpers<AgreementFormValues>
+  ) => {
+    actions.setSubmitting(true);
+    const res = await editAgreement({
+      agreementId: id.toString(),
+      body: {
+        title: values.title,
+        tokens: values.tokens,
+        unitPrice: values.tokenUnitPrice,
+        startDate: values.startDate,
+        expirationDate: values.expirationDate,
+        isTokenBased: values.isTokenBased,
+        note: values.note,
+        customerId: parseInt(values.customer),
+      },
+    });
+
+    if (res && "data" in res) {
+      refetch();
+      history.push(`/backoffice/agreements/${res.data.id}`);
+      add({ type: "success", message: "Agreement Updated" });
+      return;
+    }
+    add({
+      type: "danger",
+      message: "There was an error",
+    });
+    return;
+  };
+
   return (
     <OpsUserContainer>
       <PageTitle
@@ -22,7 +66,11 @@ const EditAgreementPage = () => {
         Edit Agreement: {currentData?.title}
       </PageTitle>
       <Card className="aq-pb-4">
-        <AgreementForm agreement={currentData} refetch={refetch} />
+        <AgreementForm
+          agreement={currentData}
+          refetch={refetch}
+          onSubmit={onSubmit}
+        />
       </Card>
     </OpsUserContainer>
   );
