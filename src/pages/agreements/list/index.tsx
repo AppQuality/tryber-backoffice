352f450c9ce43b97ea72ff9f06081de292e486f2
Marco Bonomo <marco.bonomo@unguess.io>
2023-07-06 18:57:53 +0200

    🔥 refactor(getAllAgreements.js): remove unused code and update dependencies 🆕 feat(edit.svg): add new SVG file for edit icon 🆕 feat(trash.svg): add new SVG file for trash icon 🆕 feat(Agreements.tsx): add new component for displaying agreements list 🔧 chore(FilterContext.tsx): update context name and remove unused code 🔧 chore(CustomersFilter/index.tsx): update filter name and options 🔧 chore(CustomersFilter/useCustomers.tsx): add new hook for fetching customers data and update return value
    🔥 refactor(Filters/index.tsx): remove unused filters component
    
    🔨 refactor(list/index.tsx): refactor AgreementsListPage component

--- src/pages/agreements/list/index.tsx
+++ src/pages/agreements/list/index.tsx
@@ -2,138 +2,53 @@ import {
   BSCol,
   BSGrid,
   Button,
-  Table,
   Title,
 } from "@appquality/appquality-design-system";
-import { useEffect, useState } from "react";
-import getAllAgreements from "src/api/getAllAgreements";
-import FilterContext from "src/pages/BugsList/FilterContext";
+import FilterContext from "src/pages/agreements/list/FilterContext";
 import styled from "styled-components";
 import CustomersFilter from "src/pages/agreements/list/Filters/CustomersFilter";
-
-interface Agreement {
-  id: number;
-  title: string;
-  tokens: number;
-  unitPrice: number;
-  startDate: string;
-  expirationDate: string;
-  customer: {
-    id: number;
-    company: string;
-  };
-  note: string;
-  isTokenBased: boolean;
-}
+import { Agreements } from "./Agreements";
 
 const FluidContainer = styled.div`
-    max-width: 90%;
-    margin: 0 auto;
+    padding: 2rem;
   }
 `;
 
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const HeaderButton = (props: Parameters<typeof Button>[0]) => {
-  return <Button {...props} size="sm" type="link-hover" />;
+  return <Button {...props} size="md" type="secondary" />;
 };
 
 const AgreementsListPage = () => {
-  const [agreements, setAgreements] = useState<
-    (Agreement & { key: string }[]) | []
-  >([]);
-  const columns = [
-    {
-      title: "Id",
-      dataIndex: "id",
-      key: "id",
-    },
-    {
-      title: "Title",
-      dataIndex: "title",
-      key: "title",
-    },
-    {
-      title: "Tokens",
-      dataIndex: "tokens",
-      key: "tokens",
-    },
-    {
-      title: "Unit price",
-      dataIndex: "unitPrice",
-      key: "unitPrice",
-    },
-    {
-      title: "Start date",
-      dataIndex: "startDate",
-      key: "startDate",
-    },
-    {
-      title: "Expiration date",
-      dataIndex: "expirationDate",
-      key: "expirationDate",
-    },
-    {
-      title: "Customer",
-      dataIndex: "customer",
-      key: "customer",
-    },
-    {
-      title: "Note",
-      dataIndex: "note",
-      key: "note",
-    },
-    {
-      title: "Is token based",
-      dataIndex: "isTokenBased",
-      key: "isTokenBased",
-    },
-  ];
-
-  useEffect(() => {
-    getAllAgreements()
-      .then(({ items }) => {
-        console.log(items);
-        setAgreements(
-          items.map((r: Agreement) => ({
-            id: r.id,
-            title: r.title,
-            tokens: r.tokens,
-            unitPrice: r.unitPrice,
-            startDate: r.startDate,
-            expirationDate: r.expirationDate,
-            customer: r.customer.company,
-            note: r.note,
-            isTokenBased: r.isTokenBased ? "Yes" : "No",
-            key: r.id,
-          }))
-        );
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
-  function setIsStatsModalOpen(arg0: boolean) {
-    throw new Error("Function not implemented.");
-  }
-
   return (
     <FluidContainer>
-      <Title size="m">Agreements List</Title>
-
-      <HeaderButton
-        as="a"
-        href={`/wp-admin/admin.php?page=mvc_campaigns`}
-        type="secondary"
-        className="aq-mr-2"
-      >
-        New Agreement
-      </HeaderButton>
+      <FlexContainer>
+        <Title size="m">Agreements List</Title>
+        <HeaderButton
+          as="a"
+          href={`/backoffice/agreements/new`}
+          className="aq-mr-2"
+        >
+          + Add New
+        </HeaderButton>
+      </FlexContainer>
       <FilterContext>
-        <div className="aq-mb-4">
-          <CustomersFilter id="1" />
+        <div
+          className="aq-mb-4"
+          style={{
+            margin: "1rem 0",
+          }}
+        >
+          <CustomersFilter />
         </div>
         <BSGrid>
           <BSCol size="col-lg-12">
-            <Table columns={columns} dataSource={agreements} />
+            <Agreements />
           </BSCol>
         </BSGrid>
       </FilterContext>
