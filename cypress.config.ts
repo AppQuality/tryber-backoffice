a9d3ae1a543e7566f17939334526fc98fc53da5f
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-10 17:04:36 +0200

    test(agreements): update cy config

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     env: {
       REACT_APP_API_URL: "https://dev.tryber.me/api",
       AGREEMENTS_PAGE: "/agreements",
+      AGREEMENTS_NEW_PAGE: "/agreements/new",
     },
     video: false,
     screenshotOnRunFailure: false,
