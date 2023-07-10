739e3f27add9099f6197ea46e325ffb58cd19f50
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-10 17:21:51 +0200

    test(agreements): test not logged in + implementation

--- cypress/e2e/1-agreements-list/01-unauthorized.cy.ts
+++ cypress/e2e/1-agreements-list/01-unauthorized.cy.ts
@@ -1,13 +1,13 @@
 describe("Agreements Page:", () => {
   it("If a user is not logged in, should redirect to login page (https://tryber.me/wp-login.php)", () => {
-    cy.clearCookies();
-    cy.notLoggedIn();
+    cy.intercept("GET", `${Cypress.env("AGREEMENTS_PAGE")}`, {
+      statusCode: 403,
+      fixture: "/agreements/_get/response-403-not-logged-in.json",
+    }).as("notLoggedIn");
     cy.visit(`${Cypress.env("AGREEMENTS_PAGE")}/`);
-    throw new Error("test should be implemented");
+    cy.url().should("include", "wp-login.php");
   });
   it("If a user does not have enough permissions, should see a not authorized page", () => {
-    cy.clearCookies();
-    cy.notLoggedIn();
     cy.visit(`${Cypress.env("AGREEMENTS_PAGE")}/`);
     throw new Error("test should be implemented");
   });
