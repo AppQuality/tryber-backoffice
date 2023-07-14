ccb44861878840107ec6b48634a81e8d0b1ec336
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-14 15:18:22 +0200

    fix(agreements): date picker + tests

--- cypress/e2e/3-new-agreement/02-new-form.cy.ts
+++ cypress/e2e/3-new-agreement/02-new-form.cy.ts
@@ -6,9 +6,62 @@ describe("New Single Agreement Page:", () => {
       fixture: "/customers/_get/response-200-success.json",
     }).as("getCustomers");
     cy.visit(`${Cypress.env("AGREEMENTS_PAGE")}/new`);
+    cy.wait("@getCustomers");
+  });
+  it("There should be a form with input for title", () => {
+    cy.get("input#title")
+      .should("be.visible")
+      .click()
+      .type("test")
+      .and("have.value", "test");
+  });
+  it("There should be a form with input for tokens", () => {
+    cy.get("input#isTokenBased").should("be.visible").click().and("be.checked");
+    cy.get("input#tokens")
+      .should("be.visible")
+      .click()
+      .type("1")
+      .and("have.value", "01");
+    // type 0 and have value 100 because a 0 is already present in the input when clearing the field
+    cy.get("input#tokenUnitPrice")
+      .should("be.visible")
+      .and("have.value", "165")
+      .click()
+      .clear()
+      .type("10")
+      .and("have.value", "100");
+  });
+  it("There should be a form with input for start date", () => {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.toLocaleString("default", { month: "2-digit" });
+    const year = today.getFullYear();
+    cy.dataQa("startDate").within(() => {
+      cy.get("input").should("be.visible").click();
+    });
+    cy.get(".mbsc-popup").within(() => {
+      cy.get("button:contains('Set')").click();
+    });
+    cy.dataQa("startDate").within(() => {
+      cy.get("input").should("have.value", `${day}/${month}/${year}`);
+    });
+  });
+  it("There should be a form with input for close date", () => {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.toLocaleString("default", { month: "2-digit" });
+    const year = today.getFullYear();
+    cy.dataQa("expirationDate").within(() => {
+      cy.get("input").should("be.visible").click();
+    });
+    cy.get(".mbsc-popup").within(() => {
+      cy.get("button:contains('Set')").click();
+    });
+    cy.dataQa("expirationDate").within(() => {
+      cy.get("input").should("have.value", `${day}/${month}/${year}`);
+    });
   });
   it("The customer select should be preselected with the customer field and be enabled", () => {
-    cy.wait("@getCustomers");
     cy.get("div#customer").should("contain", "Select a customer");
     // test if the customer select is not disabled after is rendered completely
     cy.get("div#customer")
@@ -17,19 +70,10 @@ describe("New Single Agreement Page:", () => {
         cy.get("input#react-select-3-input").should("be.enabled");
       });
   });
-  it("There should be a form with input for each agreement field and they should be editable, title, tokens, unit price token, start date, close date, is token based, notes, customer select", () => {
-    cy.clearCookies();
-    cy.loggedIn();
-    cy.visit(`${Cypress.env("AGREEMENTS_PAGE")}/new`);
-    cy.get("input#title").should("be.visible");
-    cy.get("input#tokens").should("be.visible");
-    cy.get("input#tokenUnitPrice").should("be.visible");
-    cy.get("input#startDate").should("be.visible");
-    cy.get("input#closeDate").should("be.visible");
-    cy.get("input#isTokenBased").should("be.visible");
+  it("There should be a form with input for notes", () => {
     cy.get("textarea#notes").should("be.visible");
-    cy.get("div#customer").should("be.visible");
   });
+
   it("The field tokenUnitPrice should be prefilled with the value 165, as a hint or suggestion", () => {});
   it("If the Agreement is NOT token based there should be a field Amount to input the total amount and calculate the number of tokens", () => {});
   it("If the Agreement is NOT token based the field tokens should be disabled", () => {});
