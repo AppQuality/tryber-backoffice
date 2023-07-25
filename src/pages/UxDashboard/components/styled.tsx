d3bd5019a5aeb284852493759584037154c3deee
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-25 17:27:08 +0200

    wip(ux-dash): add new insight

--- src/pages/UxDashboard/components/styled.tsx
+++ src/pages/UxDashboard/components/styled.tsx
@@ -3,9 +3,24 @@ import styled from "styled-components";
 export const InsightsWrapper = styled.div`
   display: grid;
   grid-auto-flow: column;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: repeat(
+    3,
+    calc(33.33% - 6px)
+  ); // account for 6px cards border widths
   gap: ${({ theme }) => theme.grid.sizes[2]};
   justify-content: flex-start;
-  background-color: ${({ theme }) => theme.colors.gray100};
-  padding: ${({ theme }) => theme.grid.sizes[2]};
+`;
+
+export const AddNewInsightCTA = styled.div`
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.gray400};
+  &:hover {
+    color: ${({ theme }) => theme.colors.gray800};
+  }
 `;
