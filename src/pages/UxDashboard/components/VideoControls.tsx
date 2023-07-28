8277dab5b10bd50cc2275073a6146c55eb489d1f
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-07-28 10:23:57 +0200

    merge

--- src/pages/UxDashboard/components/VideoControls.tsx
+++ src/pages/UxDashboard/components/VideoControls.tsx
@@ -7,26 +7,20 @@ const ProgressBar = styled(Video.ProgressBar)`
   }
   background-color: ${({ theme }) => theme.colors.purple100};
 `;
+const FlexDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+`;
+const CustomTimer = styled(Video.Timer)`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: -20px;
+  color: ${({ theme }) => theme.colors.purple600};
+  background-color: ${({ theme }) => theme.colors.purple200};
+  border-radius: 5px;
+`;
 export const VideoControls = () => {
-  const ProgressBar = styled(Video.ProgressBar)`
-    :after {
-      background-color: ${({ theme }) => theme.colors.purple600};
-    }
-    background-color: ${({ theme }) => theme.colors.purple100};
-  `;
-  const FlexDiv = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 5px;
-  `;
-  const CustomTimer = styled(Video.Timer)`
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: -20px;
-    color: ${({ theme }) => theme.colors.purple600};
-    background-color: ${({ theme }) => theme.colors.purple200};
-    border-radius: 5px;
-  `;
   return (
     <>
       <FlexDiv>
