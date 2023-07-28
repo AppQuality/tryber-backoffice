8e3fd997329f37e4fbd20a4d5a31fae56cd6509c
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-28 12:20:13 +0200

    feat(ux-dash): preview iframe

--- src/pages/UxDashboard/components/Preview.tsx
+++ src/pages/UxDashboard/components/Preview.tsx
@@ -1,78 +1,22 @@
+import styled from "styled-components";
+
+const StyledIframe = styled.iframe`
+  width: 100%;
+  height: 100vh;
+  border: none;
+`;
+
 export const Preview = () => {
+  const origin =
+    window.location.origin.includes("localhost") ||
+    window.location.origin.includes("dev.")
+      ? "https://dev.unguess.io"
+      : "https://unguess.io";
   return (
-    <div data-qa="ux-dashboard-preview">
-      Preview Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-      quos <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos{" "}
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos{" "}
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos{" "}
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos{" "}
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos{" "}
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-      Quisquam, quos voluptatem. Quisquam, quos voluptatem. Quisquam, quos
-      <br />
-      voluptatem. Quisquam, quos voluptatem. Quisquam, quos voluptatem.
-      <br />
-    </div>
+    <StyledIframe
+      title="preview"
+      src={`${origin}/campaigns/4845/preview`}
+      data-qa="ux-dashboard-preview"
+    />
   );
 };
