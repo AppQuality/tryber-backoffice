bd2431d80582a95c7286ab08812a498a19b137dc
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-07-28 10:53:14 +0200

    wip: fullscreen

--- src/pages/UxDashboard/components/VideoControls.tsx
+++ src/pages/UxDashboard/components/VideoControls.tsx
@@ -15,18 +15,47 @@ const FlexDiv = styled.div`
 const CustomTimer = styled(Video.Timer)`
   margin-left: auto;
   margin-right: auto;
-  margin-top: -20px;
-  color: ${({ theme }) => theme.colors.purple600};
+  margin-top: -31px;
+  color: ${({ theme }) => theme.colors.black};
   background-color: ${({ theme }) => theme.colors.purple200};
   border-radius: 5px;
+  padding: 5px;
 `;
+
+const Content = styled.div<{
+  isFullScreen?: boolean;
+}>`
+  position: relative;
+  display: flex;
+
+  ${({ isFullScreen }) =>
+    isFullScreen &&
+    `
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    `}
+`;
+
+export const VideoContent = ({
+  children,
+}: {
+  children: React.ReactNode | React.ReactNodeArray;
+}) => {
+  const { isFullScreen } = VideoContent();
+
+  return <Content isFullScreen={isFullScreen}>{children}</Content>;
+};
+
 export const VideoControls = () => {
   return (
     <>
       <FlexDiv>
         <CustomTimer className={"timer"} />
       </FlexDiv>
-
+      <Video.FullScreen></Video.FullScreen>
       <FlexDiv>
         <Video.ChangeTime value={-10}></Video.ChangeTime>
         <Video.PlayPauseButton className="play-pause-button" />
