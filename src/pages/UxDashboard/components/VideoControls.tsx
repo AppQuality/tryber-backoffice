0d1ba704589303765cdcc46bf6776d146b4afcbb
Iacopo Leardini <iacopo.lea@gmail.com>
2023-07-31 17:38:06 +0200

    feat(ux-dash): player

--- src/pages/UxDashboard/components/VideoControls.tsx
+++ src/pages/UxDashboard/components/VideoControls.tsx
@@ -5,13 +5,15 @@ import { useFormikContext } from "formik";
 import {
   ArrowCounterclockwise,
   ArrowClockwise,
-  ClockHistory,
+  Scissors,
   PlayCircleFill,
   PauseCircleFill,
   ArrowsFullscreen,
   FullscreenExit,
   VolumeMuteFill,
   VolumeUpFill,
+  PlayFill,
+  PauseFill,
 } from "react-bootstrap-icons";
 
 const ProgressBar = styled(Video.ProgressBar)`
@@ -20,30 +22,59 @@ const ProgressBar = styled(Video.ProgressBar)`
   }
   background-color: ${({ theme }) => theme.colors.purple100};
 `;
-const FlexDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 5px;
-`;
 const CustomTimer = styled(Video.Timer)`
   margin-left: auto;
-  margin-right: auto;
-  margin-top: -31px;
-  color: ${({ theme }) => theme.colors.black};
-  background-color: ${({ theme }) => theme.colors.purple200};
-  border-radius: 5px;
-  padding: 5px;
+  margin-right: 0;
+  color: ${({ theme }) => theme.colors.white};
 `;
-const PlayPauseButton = styled(Video.PlayPauseButton)``;
 const MuteButton = styled(Video.Mute)``;
 
-const CenteredPlayPause = styled(Video.PlayPauseButton)`
-  background-color: ${({ theme }) => theme.colors.purple200};
-  border-radius: 5px;
-  padding: 5px;
-  margin: 0 5px;
+const ControlsWrapper = styled.div`
+  background-color: rgba(0, 0, 0, 0.65);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 5px;
+  opacity: 0;
+  transition: opacity 0.2s;
   position: absolute;
-  top: 50%;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  svg {
+    fill: white;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+  }
+  :hover {
+    opacity: 1;
+  }
+`;
+
+const CenteredPlayPause = styled(Video.PlayPauseButton)`
+  flex: 1 0 auto;
+  border-radius: 0;
+  margin: 0;
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  padding: 40px 0;
+  svg {
+    width: 50px;
+    height: 50px;
+    fill: white;
+  }
+`;
+
+const OtherControls = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `;
 const ChangeTime = styled(Video.ChangeTime)``;
 const FullScreen = styled(Video.FullScreen)``;
@@ -65,31 +96,26 @@ export const VideoControls = ({
   };
 
   return (
-    <>
-      <FlexDiv style={{ backgroundColor: "green" }}>
-        <CustomTimer className={"timer"} />
-      </FlexDiv>
-      <FlexDiv style={{ backgroundColor: "red" }}>
+    <ControlsWrapper>
+      <CenteredPlayPause
+        i18n={{ play: <PlayCircleFill />, pause: <PauseCircleFill /> }}
+      />
+      <CustomTimer className={"timer"} />
+      <ProgressBar className="progress-bar" />
+      <OtherControls>
+        <ChangeTime value={-10} i18n={{ icon: <ArrowCounterclockwise /> }} />
+        <MuteButton i18n={{ on: <VolumeUpFill />, off: <VolumeMuteFill /> }} />
+        <Video.PlayPauseButton
+          i18n={{ play: <PlayFill />, pause: <PauseFill /> }}
+        />
+        <Button onClick={fillEndTime}>
+          <Scissors />
+        </Button>
+        <ChangeTime value={10} i18n={{ icon: <ArrowClockwise /> }} />
         <FullScreen
           i18n={{ enter: <ArrowsFullscreen />, exit: <FullscreenExit /> }}
         />
-
-        <ChangeTime value={-10} i18n={{ icon: <ArrowCounterclockwise /> }} />
-        <CenteredPlayPause
-          className="play-pause-button"
-          i18n={{ play: <PlayCircleFill />, pause: <PauseCircleFill /> }}
-        />
-        <PlayPauseButton
-          className="play-pause-button"
-          i18n={{ play: <PlayCircleFill />, pause: <PauseCircleFill /> }}
-        />
-        <Button onClick={fillEndTime}>
-          <ClockHistory />
-        </Button>
-        <ChangeTime value={10} i18n={{ icon: <ArrowClockwise /> }} />
-        <MuteButton i18n={{ on: <VolumeUpFill />, off: <VolumeMuteFill /> }} />
-      </FlexDiv>
-      <ProgressBar className="progress-bar" />
-    </>
+      </OtherControls>
+    </ControlsWrapper>
   );
 };
