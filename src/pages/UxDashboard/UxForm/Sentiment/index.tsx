0bb37f1c2f03c25fa22cb0bb6495865a4b51e046
Davide Bizzi <davide.bizzi@unguess.io>
2023-09-04 10:26:48 +0200

    feat: Add sentiment form delete

--- src/pages/UxDashboard/UxForm/Sentiment/index.tsx
+++ src/pages/UxDashboard/UxForm/Sentiment/index.tsx
@@ -3,6 +3,7 @@ import { useFormikContext } from "formik";
 import { useParams } from "react-router-dom";
 import { useGetCampaignsByCampaignClustersQuery } from "src/services/tryberApi";
 import { useAppDispatch } from "src/store";
+import styled from "styled-components";
 import {
   setSentimentDeleteModalOpen,
   setSentimentModalOpen,
@@ -15,6 +16,13 @@ import SentimentChartModal from "./SentimentModal";
 
 export const fieldName = "sentiments";
 
+const SentimentSectionWrapper = styled.div`
+  display: flex;
+
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const SentimentSection = () => {
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
@@ -24,17 +32,20 @@ const SentimentSection = () => {
   return (
     <Card
       title={
-        <div data-qa="sentiment-chart-section-title">
+        <SentimentSectionWrapper data-qa="sentiment-chart-section-title">
           <div>Sentiment</div>
           <div>
             <Button
+              flat
+              size="sm"
+              type="danger"
               data-qa="delete-sentiment-chart-button"
               onClick={() => dispatch(setSentimentDeleteModalOpen(true))}
             >
               Delete
             </Button>
           </div>
-        </div>
+        </SentimentSectionWrapper>
       }
       className="aq-mb-3"
       data-qa="sentiment-chart-section"
