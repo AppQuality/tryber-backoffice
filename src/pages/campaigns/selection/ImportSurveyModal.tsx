a2f1294fb03ba8f2399971dddded41031735178c
Iacopo Leardini <iacopo.lea@gmail.com>
2024-02-29 17:14:53 +0100

    wip(selection): Jotforms API queries to ImportSurveyModal

--- src/pages/campaigns/selection/ImportSurveyModal.tsx
+++ src/pages/campaigns/selection/ImportSurveyModal.tsx
@@ -1,6 +1,10 @@
 import { Button, Modal, Select } from "@appquality/appquality-design-system";
 import { useAppSelector, useAppDispatch } from "src/store";
 import { closeSurveyModal } from "./selectionSlice";
+import {
+  useGetJotformsFormsByFormIdQuestionsQuery,
+  useGetJotformsFormsQuery,
+} from "src/services/tryberApi";
 
 const ImportSurveyModal = () => {
   const { isSurveyModalOpen } = useAppSelector((state) => state.selection);
@@ -8,6 +12,10 @@ const ImportSurveyModal = () => {
   const close = () => {
     dispatch(closeSurveyModal());
   };
+  const { data: jotforms } = useGetJotformsFormsQuery();
+  const { data: questions } = useGetJotformsFormsByFormIdQuestionsQuery({
+    formId: "formId",
+  });
   const emptyOption = { label: "empty", value: "empty" };
   return (
     <Modal
