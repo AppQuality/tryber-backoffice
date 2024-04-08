44ffcff3afd7a4008df9cfb0d5dd674e31ae62f2
Davide Bizzi <davide.bizzi@unguess.io>
2024-04-08 12:17:38 +0200

    fix: Add pagetemplate in page

--- src/pages/campaigns/index.tsx
+++ src/pages/campaigns/index.tsx
@@ -1,13 +1,16 @@
+import { PageTemplate } from "src/features/PageTemplate";
 import CampaignsTable from "./components/table";
 import FiltersContext from "./components/table/FilterContext";
 
 const Campaigns = () => {
   return (
-    <div style={{ background: "#e5e6e6", float: "left", width: "100%" }}>
-      <FiltersContext>
-        <CampaignsTable />
-      </FiltersContext>
-    </div>
+    <PageTemplate>
+      <div style={{ background: "#e5e6e6", float: "left", width: "100%" }}>
+        <FiltersContext>
+          <CampaignsTable />
+        </FiltersContext>
+      </div>
+    </PageTemplate>
   );
 };
 export default Campaigns;
