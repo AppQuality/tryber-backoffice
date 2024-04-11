813972ec7e9da5a0a28adce244b988dba96cc01c
Davide Bizzi <davide.bizzi@unguess.io>
2024-04-11 12:58:21 +0200

    feat: Add dateinput

---
+++ src/pages/campaigns/new/components/DateInput.tsx
@@ -0,0 +1,46 @@
+import { FormEventHandler } from "react";
+import { styled } from "styled-components";
+
+const StyledInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  label {
+    margin-bottom: 0.5rem;
+  }
+  input {
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 1rem;
+  }
+`;
+
+const DateInput = ({
+  name,
+  value,
+  label,
+  onChange,
+}: {
+  name: string;
+  value: string;
+  label: string;
+  onChange: (value: string) => void;
+}) => {
+  const handleChange: FormEventHandler<HTMLInputElement> = (e) => {
+    onChange(e.currentTarget.value);
+  };
+  return (
+    <StyledInput>
+      <label htmlFor={name}>{label}</label>
+      <input
+        value={value}
+        type="date"
+        name={name}
+        id={name}
+        onChange={handleChange}
+      />
+    </StyledInput>
+  );
+};
+export default DateInput;
