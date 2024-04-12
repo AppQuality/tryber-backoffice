52507fe91b6cd2fea0613749b2ee32a74f1a49b0
Iacopo Leardini <iacopo.lea@gmail.com>
2024-04-12 18:12:29 +0200

    Refactor form component to use dossier data

--- src/pages/campaigns/edit/index.tsx
+++ src/pages/campaigns/edit/index.tsx
@@ -1,10 +1,15 @@
+import { useParams } from "react-router-dom";
 import { PageTemplate } from "src/features/PageTemplate";
 import Form from "src/pages/campaigns/components/form";
+import { useGetDossiersByCampaignQuery } from "src/services/tryberApi";
 
 const EditCampaign = () => {
+  const { id } = useParams<{ id: string }>();
+  const { data } = useGetDossiersByCampaignQuery({ campaign: id });
+  console.log(data);
   return (
     <PageTemplate type="unguess">
-      <Form />
+      <Form dossier={data} />
     </PageTemplate>
   );
 };
