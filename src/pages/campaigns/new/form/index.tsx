b8af25401a33334aa411028fbe5c745316f3ddb0
Davide Bizzi <davide.bizzi@unguess.io>
2024-04-12 13:22:08 +0200

    feat: Add submit

--- src/pages/campaigns/new/form/index.tsx
+++ src/pages/campaigns/new/form/index.tsx
@@ -2,24 +2,29 @@ import { useFormikContext } from "formik";
 import FormProvider, { NewCampaignValues } from "./FormProvider";
 import CsmSelect from "./fields/CsmSelect";
 import CustomerSelect from "./fields/CustomerSelect";
+import CustomerTitleInput from "./fields/CustomerTitleInput";
 import DeviceMultiselect from "./fields/DeviceMultiselect";
 import StartDatePicker from "./fields/StartDatePicker";
 import TestTypeSelect from "./fields/TestTypeSelect";
 import TesterTitleInput from "./fields/TesterTitleInput";
 
 const FormContent = () => {
-  const { values } = useFormikContext<NewCampaignValues>();
+  const { values, submitForm, errors } = useFormikContext<NewCampaignValues>();
 
   return (
     <>
       {JSON.stringify(values)}
+      {JSON.stringify(errors)}
       <CustomerSelect />
       <CsmSelect />
       <TestTypeSelect />
       <TesterTitleInput />
+      <CustomerTitleInput />
 
       <StartDatePicker />
       <DeviceMultiselect />
+
+      <button type="submit">Submit</button>
     </>
   );
 };
