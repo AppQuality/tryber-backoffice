61aee5b46f5b2db7d835d4388aadb1e990618c11
Iacopo Leardini <iacopo.lea@gmail.com>
2024-04-22 09:43:05 +0200

    Update country selection field in campaign form

--- src/pages/campaigns/components/campaignForm/fields/CountrySelect.tsx
+++ src/pages/campaigns/components/campaignForm/fields/CountrySelect.tsx
@@ -2,14 +2,18 @@ import { FieldProps, Field as FormikField, useFormikContext } from "formik";
 import { NewCampaignValues } from "../FormProvider";
 import countries from "i18n-iso-countries";
 import { useMemo } from "react";
-import { ErrorMessage, Dropdown } from "@appquality/appquality-design-system";
+import {
+  ErrorMessage,
+  Dropdown,
+  FormGroup,
+  FormLabel,
+} from "@appquality/appquality-design-system";
 
 interface Option {
   label: string;
   value: string;
 }
 const CountrySelect = () => {
-  const { values, setFieldValue } = useFormikContext<NewCampaignValues>();
   const countriesOptions = useMemo(() => {
     countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
     const enCountries = countries.getNames("en");
@@ -22,21 +26,28 @@ const CountrySelect = () => {
 
   return (
     <FormikField name="countries">
-      {({ field }: FieldProps<NewCampaignValues["countries"]>) => (
-        <>
+      {({ field, form }: FieldProps<NewCampaignValues["countries"]>) => (
+        <FormGroup>
+          <FormLabel htmlFor={field.name} label="Countries" />
           <Dropdown
             placeholder=""
             data-qa="country-select"
             isMulti
             name={field.name}
+            id={field.name}
             options={countriesOptions}
             value={countriesOptions.filter((o) =>
               field.value.includes(o.value)
             )}
-            onChange={(options) => {}}
+            onChange={(v) => {
+              form.setFieldValue(
+                field.name,
+                v.map((o) => o.value)
+              );
+            }}
           />
           <ErrorMessage name={field.name} />
-        </>
+        </FormGroup>
       )}
     </FormikField>
   );
