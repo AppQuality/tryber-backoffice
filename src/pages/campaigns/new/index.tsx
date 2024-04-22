b1ed4fb09484e07fa10d994b4022769dfc68f6a7
Davide Bizzi <davide.bizzi@unguess.io>
2024-04-22 11:10:16 +0200

    feat: Allow duplication

--- src/pages/campaigns/new/index.tsx
+++ src/pages/campaigns/new/index.tsx
@@ -1,12 +1,31 @@
+import { useMemo } from "react";
+import { useLocation } from "react-router-dom";
 import { PageTemplate } from "src/features/PageTemplate";
+import { useGetDossiersByCampaignQuery } from "src/services/tryberApi";
 import CampaignForm from "../components/campaignForm";
 
 const NewCampaign = () => {
+  const { search } = useLocation();
+  const searchParams = useMemo(() => new URLSearchParams(search), [search]);
+  const duplicateFrom = searchParams.get("duplicateFrom");
   return (
     <PageTemplate>
-      <CampaignForm />
+      {duplicateFrom ? (
+        <DuplicateCampaignForm dossierId={duplicateFrom} />
+      ) : (
+        <CampaignForm />
+      )}
     </PageTemplate>
   );
+};
+
+const DuplicateCampaignForm = ({ dossierId }: { dossierId: string }) => {
+  const { data, isLoading } = useGetDossiersByCampaignQuery({
+    campaign: dossierId,
+  });
+  if (isLoading || !data) return <div>Loading...</div>;
+
+  return <CampaignForm dossier={data} />;
 };
 
 export default NewCampaign;
