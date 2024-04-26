1b90e86fff7a499c9f8769c7d437dc14bb471920
Davide Bizzi <davide.bizzi@unguess.io>
2024-04-26 16:34:58 +0200

    feat: Add all option to language country and browser

--- src/pages/campaigns/components/campaignForm/fields/CountrySelect.tsx
+++ src/pages/campaigns/components/campaignForm/fields/CountrySelect.tsx
@@ -1,18 +1,24 @@
-import { FieldProps, Field as FormikField } from "formik";
-import { NewCampaignValues } from "../FormProvider";
-import countries from "i18n-iso-countries";
-import { useMemo } from "react";
 import {
-  ErrorMessage,
   Dropdown,
+  ErrorMessage,
   FormGroup,
   FormLabel,
 } from "@appquality/appquality-design-system";
+import { FieldProps, Field as FormikField, useFormikContext } from "formik";
+import countryList from "i18n-iso-countries";
+import { useCallback, useMemo } from "react";
+import { NewCampaignValues } from "../FormProvider";
 
 const CountrySelect = () => {
-  const countriesOptions = useMemo(() => {
-    countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
-    const enCountries = countries.getNames("en");
+  const fieldName = "countries";
+  const {
+    values: { countries },
+    setFieldValue,
+  } = useFormikContext<NewCampaignValues>();
+
+  const options = useMemo(() => {
+    countryList.registerLocale(require("i18n-iso-countries/langs/en.json"));
+    const enCountries = countryList.getNames("en");
     return Object.entries(enCountries).map(([id, label]) => ({
       id: id,
       value: id,
@@ -20,25 +26,61 @@ const CountrySelect = () => {
     }));
   }, []);
 
+  const allOption = useMemo(
+    () => ({ value: "all", label: `All Countries` }),
+    []
+  );
+  const getOptions = () => {
+    return [allOption, ...options];
+  };
+
+  const getValue = useCallback(() => {
+    const values = options.filter(
+      (o) => countries && countries.includes(o.value)
+    );
+    if (options.length === values.length) return [allOption];
+    return values;
+  }, [countries, options, allOption]);
+
+  const handleChange = useCallback(
+    (selectedOptions: Readonly<{ label: string; value: string }[]>) => {
+      if (selectedOptions === null || selectedOptions === undefined) {
+        setFieldValue(fieldName, []);
+        return;
+      }
+      const currentValue = getValue();
+
+      if (
+        !currentValue.some((opt) => opt.value === allOption.value) &&
+        selectedOptions.some((opt) => opt.value === allOption.value)
+      ) {
+        selectedOptions = options;
+      }
+
+      selectedOptions = selectedOptions.filter(
+        (opt) => opt.value !== allOption.value
+      );
+
+      setFieldValue(
+        fieldName,
+        selectedOptions.map((o) => o.value)
+      );
+    },
+    [setFieldValue, allOption.value, getValue, options]
+  );
+
   return (
     <FormikField name="countries">
-      {({ field, form }: FieldProps<NewCampaignValues["countries"]>) => (
+      {({ field }: FieldProps<NewCampaignValues["countries"]>) => (
         <FormGroup>
           <FormLabel htmlFor={field.name} label="Countries" />
           <Dropdown
             isMulti
             name={field.name}
             id={field.name}
-            options={countriesOptions}
-            value={countriesOptions.filter((o) =>
-              field.value.includes(o.value)
-            )}
-            onChange={(v) => {
-              form.setFieldValue(
-                field.name,
-                v.map((o) => o.value)
-              );
-            }}
+            options={getOptions()}
+            value={getValue()}
+            onChange={handleChange}
           />
           <ErrorMessage name={field.name} />
         </FormGroup>
--- src/pages/campaigns/components/campaignForm/fields/LanguagesSelect.tsx
+++ src/pages/campaigns/components/campaignForm/fields/LanguagesSelect.tsx
@@ -1,22 +1,92 @@
+import {
+  Dropdown,
+  ErrorMessage,
+  FormGroup,
+  FormLabel,
+} from "@appquality/appquality-design-system";
+import { FieldProps, Field as FormikField, useFormikContext } from "formik";
+import { useCallback, useMemo } from "react";
 import { useGetLanguagesQuery } from "src/services/tryberApi";
-import { useMemo } from "react";
-import { SelectField } from "./SelectField";
+import { NewCampaignValues } from "../FormProvider";
 
 const LanguageSelect = () => {
-  const { data: languages } = useGetLanguagesQuery();
+  const fieldName = "languages";
+  const { data: languageList } = useGetLanguagesQuery();
+  const {
+    values: { languages },
+    setFieldValue,
+  } = useFormikContext<NewCampaignValues>();
 
   const options = useMemo(
     () =>
-      languages?.map((language) => ({
+      languageList?.map((language) => ({
         id: language.id.toString(),
         label: language.name,
         value: language.id.toString(),
       })) || [],
-    [languages]
+    [languageList]
   );
 
+  const allOption = useMemo(
+    () => ({ value: "all", label: `All Languages` }),
+    []
+  );
+
+  const getOptions = () => {
+    return [allOption, ...options];
+  };
+
+  const getValue = useCallback(() => {
+    const values = options.filter(
+      (o) => languages && languages.includes(o.value)
+    );
+    if (options.length === values.length) return [allOption];
+    return values;
+  }, [languages, options, allOption]);
+
+  const handleChange = useCallback(
+    (selectedOptions: Readonly<{ label: string; value: string }[]>) => {
+      if (selectedOptions === null || selectedOptions === undefined) {
+        setFieldValue(fieldName, []);
+        return;
+      }
+      const currentValue = getValue();
+
+      if (
+        !currentValue.some((opt) => opt.value === allOption.value) &&
+        selectedOptions.some((opt) => opt.value === allOption.value)
+      ) {
+        selectedOptions = options;
+      }
+
+      selectedOptions = selectedOptions.filter(
+        (opt) => opt.value !== allOption.value
+      );
+
+      setFieldValue(
+        fieldName,
+        selectedOptions.map((o) => o.value)
+      );
+    },
+    [setFieldValue, allOption.value, getValue, options]
+  );
   return (
-    <SelectField name="languages" label="Languages" isMulti options={options} />
+    <FormikField name="languages">
+      {({ field }: FieldProps) => (
+        <FormGroup>
+          <FormLabel htmlFor={field.name} label="Languages" />
+          <Dropdown
+            name={field.name}
+            id={field.name}
+            options={getOptions()}
+            isMulti
+            value={getValue()}
+            onChange={handleChange}
+          />
+          <ErrorMessage name={field.name} />
+        </FormGroup>
+      )}
+    </FormikField>
   );
 };
 
