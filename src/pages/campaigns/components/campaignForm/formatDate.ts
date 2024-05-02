155008ca14cf078ac835a6507bc5743a939d55db
Iacopo Leardini <iacopo.lea@gmail.com>
2024-05-02 15:15:11 +0200

    Refactor formatDate function to ignore TZ

--- src/pages/campaigns/components/campaignForm/formatDate.ts
+++ src/pages/campaigns/components/campaignForm/formatDate.ts
@@ -2,11 +2,12 @@
  * convert a date string formatted like this 2004-01-08T00:00:00Z to a date suitable for a input type date "yyyy-mm-dd"
  */
 export const formatDate = (date: string) => {
-  const dateObj = new Date(date);
-  const year = dateObj.getFullYear();
-  const month = `0${dateObj.getMonth() + 1}`.slice(-2);
-  const day = `0${dateObj.getDate()}`.slice(-2);
-  return `${year}-${month}-${day}`;
+  // const dateObj = new Date(date);
+  // const year = dateObj.getFullYear();
+  // const month = `0${dateObj.getMonth() + 1}`.slice(-2);
+  // const day = `0${dateObj.getDate()}`.slice(-2);
+  // return `${year}-${month}-${day}`;
+  return date.split("T")[0];
 };
 
 export const formatTime = (date: string) => {
