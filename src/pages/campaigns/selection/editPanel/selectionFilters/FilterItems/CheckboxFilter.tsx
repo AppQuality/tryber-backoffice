33b62fd68916b3e389b745c5b6589d8a0775189c
Iacopo Leardini <iacopo.lea@gmail.com>
2024-08-05 16:55:56 +0200

    feat: mark invalid answers

--- src/pages/campaigns/selection/editPanel/selectionFilters/FilterItems/CheckboxFilter.tsx
+++ src/pages/campaigns/selection/editPanel/selectionFilters/FilterItems/CheckboxFilter.tsx
@@ -2,6 +2,7 @@ import { Checkbox, FormLabel } from "@appquality/appquality-design-system";
 import { useAppDispatch } from "src/store";
 import { changeTablePage, setFilters } from "../../../selectionSlice";
 import { FilterContainer } from "./components/FilterContainer";
+import { useMemo } from "react";
 
 const CheckboxFilter = ({
   title,
@@ -12,27 +13,55 @@ const CheckboxFilter = ({
 }: {
   title: string;
   key: string;
-  options: string[];
+  options: CustomQuestionOption[];
   selected?: string[];
   onSelect: (checked: boolean, option: string) => ReturnType<typeof setFilters>;
 }) => {
   const dispatch = useAppDispatch();
+  const hasInvalidAnswer = useMemo(
+    () => options.some((o) => o.isInvalid),
+    [options]
+  );
+  const questionLabel = useMemo(
+    () => (
+      <>
+        <span>{title}</span>
+        {hasInvalidAnswer && (
+          <small className="aq-text-danger"> (domanda a sbarramento)</small>
+        )}
+      </>
+    ),
+    [title, hasInvalidAnswer]
+  );
+
   return (
     <FilterContainer>
-      <FormLabel htmlFor={`filters.${key}`} label={title} />
-      {options.map((d) => (
-        <div key={d}>
-          <Checkbox
-            name={`filters.${key}.${d}`}
-            label={d}
-            checked={selected ? selected.includes(d) : undefined}
-            onChange={(e) => {
-              dispatch(changeTablePage({ newPage: 1 }));
-              dispatch(onSelect(e.target.checked, d));
-            }}
-          />
-        </div>
-      ))}
+      <FormLabel htmlFor={`filters.${key}`} label={questionLabel} />
+      {options.map((d) => {
+        const { value } = d;
+        return (
+          <div key={value}>
+            <Checkbox
+              name={`filters.${key}.${value}`}
+              label={
+                <>
+                  <span>{value}</span>
+                  {d.isInvalid && (
+                    <small className="aq-text-danger"> (*)</small>
+                  )}
+                </>
+              }
+              checked={
+                selected ? selected.includes(value.toString()) : undefined
+              }
+              onChange={(e) => {
+                dispatch(changeTablePage({ newPage: 1 }));
+                dispatch(onSelect(e.target.checked, value.toString()));
+              }}
+            />
+          </div>
+        );
+      })}
     </FilterContainer>
   );
 };
