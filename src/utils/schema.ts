1b03de3b9692665cd83ae171b7a905c2c4146601
Kariamos <minucci9292@gmail.com>
2024-10-15 18:26:25 +0200

    (wip) refactor: Update language handling in FormProvider and schema.ts

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -805,19 +805,17 @@ export interface components {
       short_name?: string;
     } & (
       | {
-          /** @enum {string} */
-          type: "text" | "gender" | "phone_number" | "address";
+          type: components["schemas"]["PreselectionQuestionSimple"];
         }
       | {
-          /** @enum {string} */
-          type: "multiselect" | "select" | "radio";
-          options: {
+          type: components["schemas"]["PreselectionQuestionMultiple"];
+          options?: {
             value: string;
             isInvalid?: boolean;
           }[];
         }
       | {
-          type: string;
+          type: components["schemas"]["PreselectionQuestionCuf"];
           options?: {
             value: number;
             isInvalid?: boolean;
@@ -942,11 +940,23 @@ export interface components {
         cap?: number;
       };
       countries?: components["schemas"]["CountryCode"][];
-      languages?: number[];
+      languages?: string[];
       browsers?: number[];
       productType?: number;
       notes?: string;
     };
+    /**
+     * PreselectionQuestionSimple
+     * @enum {string}
+     */
+    PreselectionQuestionSimple: "gender" | "text" | "phone_number" | "address";
+    /**
+     * PreselectionQuestionMultiple
+     * @enum {string}
+     */
+    PreselectionQuestionMultiple: "multiselect" | "select" | "radio";
+    /** PreselectionQuestionCuf */
+    PreselectionQuestionCuf: string;
   };
   responses: {
     /** A user */
@@ -1966,8 +1976,6 @@ export interface operations {
       200: {
         content: {
           "application/json": {
-            /** @enum {string} */
-            status: "draft" | "published" | "draft-modified";
             goal: string;
             usersNumber: number;
             sentiments: {
@@ -1989,6 +1997,7 @@ export interface operations {
               id: number;
               name: string;
             }[];
+            visible: number;
           };
         };
       };
@@ -2015,30 +2024,25 @@ export interface operations {
     };
     requestBody: {
       content: {
-        "application/json":
-          | {
-              goal: string;
-              usersNumber: number;
-              sentiments: {
-                id?: number;
-                clusterId: number;
-                value: number;
-                comment: string;
-              }[];
-              methodology: {
-                /** @enum {string} */
-                type: "qualitative" | "quantitative" | "quali-quantitative";
-                description: string;
-              };
-              questions: {
-                id?: number;
-                name: string;
-              }[];
-            }
-          | {
-              /** @enum {string} */
-              status: "publish";
-            };
+        "application/json": {
+          goal?: string;
+          usersNumber?: number;
+          visible?: number;
+          methodology?: {
+            description: string;
+            type: string;
+          };
+          sentiments?: {
+            clusterId: number;
+            value: number;
+            comment: string;
+            id?: number;
+          }[];
+          questions?: {
+            name: string;
+            id?: number;
+          }[];
+        };
       };
     };
   };
@@ -3305,7 +3309,9 @@ export interface operations {
       /** OK */
       200: {
         content: {
-          "application/json": (components["schemas"]["PreselectionFormQuestion"] & {
+          "application/json": ({
+            question: string;
+            short_name?: string;
             value?:
               | number
               | {
@@ -3319,7 +3325,19 @@ export interface operations {
               error?: string;
             };
             id: number;
-          })[];
+          } & (
+            | {
+                type: components["schemas"]["PreselectionQuestionSimple"];
+              }
+            | {
+                type: components["schemas"]["PreselectionQuestionMultiple"];
+                options: string[];
+              }
+            | {
+                type: components["schemas"]["PreselectionQuestionCuf"];
+                options?: number[];
+              }
+          ))[];
         };
       };
       403: components["responses"]["NotAuthorized"];
@@ -4195,7 +4213,6 @@ export interface operations {
             };
             countries?: components["schemas"]["CountryCode"][];
             languages?: {
-              id: number;
               name: string;
             }[];
             browsers?: {
