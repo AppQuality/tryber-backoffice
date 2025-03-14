4e7f3324426c534a6ea4f7538728f5cd959dc206
Davide Bizzi <davide.bizzi@unguess.io>
2025-03-14 11:46:24 +0100

    feat(tryberApi): add mutations for posting dossiers by campaign (manual and preview)

--- src/pages/campaigns/components/campaignForm/ImportPages/index.tsx
+++ src/pages/campaigns/components/campaignForm/ImportPages/index.tsx
@@ -9,197 +9,216 @@ import {
 } from "@appquality/appquality-design-system";
 import { useFormikContext } from "formik";
 import { useState } from "react";
+import {
+  usePostDossiersByCampaignManualMutation,
+  usePostDossiersByCampaignPreviewMutation,
+} from "src/services/tryberApi";
 import * as yup from "yup";
 import { FormProps } from "..";
 import { NewCampaignValues } from "../FormProvider";
 import { CampaignSelect } from "./CampaignSelect";
 
-type ImportPagesValues = {
-  previewAction: "generate" | "import" | "no-action";
-  previewCampaign?: number;
-  manualAction: "generate" | "import" | "no-action";
-  manualCampaign?: number;
-};
+type ImportPagesValues = (
+  | {
+      previewAction: "generate" | "no-action";
+      previewCampaign: undefined;
+    }
+  | {
+      previewAction: "import";
+      previewCampaign: number;
+    }
+) &
+  (
+    | {
+        manualAction: "generate" | "no-action";
+        manualCampaign: undefined;
+      }
+    | {
+        manualAction: "import";
+        manualCampaign: number;
+      }
+  );
 
-const ImportPagesModal = ({
-  isOpen,
-  onClose,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) => {
+const ImportPagesModal = ({ onClose }: { onClose: () => void }) => {
   const {
     values,
     setFieldValue,
     submitForm,
     setFieldTouched,
     errors,
-    touched,
     validateForm,
     isSubmitting,
-    setSubmitting,
   } = useFormikContext<ImportPagesValues>();
 
   return (
-    <Modal isOpen={isOpen} title="Import page" onClose={onClose} size="mid">
-      <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-        <div>
-          <Select
-            name="previewAction"
-            onBlur={() => {
-              setFieldTouched("previewAction");
-            }}
-            label="Preview"
-            value={
-              values?.previewAction
-                ? { label: "", value: values?.previewAction }
-                : { label: "Nessuna azione", value: "no-action" }
-            }
-            options={[
-              { label: "Genera dal tipo", value: "generate" },
-              { label: "Importa da un'altra campagna", value: "import" },
-              { label: "Nessuna azione", value: "no-action" },
-            ]}
-            menuTargetQuery={"body"}
-            onChange={(option) => {
-              if (option === null || option.value === "no-action") {
-                setFieldValue("previewAction", "no-action");
-                setFieldValue("previewCampaign", undefined);
-                return;
-              }
-
-              setFieldValue("previewAction", option.value);
-            }}
-          />
-          {values?.previewAction === "import" && (
-            <FormikField name="previewCampaign">
-              {({ field, form }: FieldProps) => (
-                <div className="aq-mt-2">
-                  <CampaignSelect
-                    onBlur={() => {
-                      form.setFieldTouched("previewCampaign");
-                    }}
-                    name={field.name}
-                    label="Campagna da cui importare la preview"
-                    placeholder="Seleziona la campagna da cui importare la preview"
-                    onChange={(value) => {
-                      if (value === null || value.value === "") {
-                        form.setFieldValue("previewCampaign", undefined);
-                        return;
-                      }
-                      form.setFieldValue(
-                        "previewCampaign",
-                        Number(value.value)
-                      );
-                    }}
-                  />
-                  {errors.previewCampaign && (
-                    <ErrorMessageWrapper>
-                      {errors.previewCampaign}
-                    </ErrorMessageWrapper>
-                  )}
-                </div>
-              )}
-            </FormikField>
-          )}
-        </div>
-        <div>
-          <Select
-            name="manualAction"
-            label="Manual"
-            onBlur={() => {
-              setFieldTouched("manualAction");
-            }}
-            value={
-              values?.manualAction
-                ? { label: "", value: values?.manualAction }
-                : { label: "Nessuna azione", value: "no-action" }
-            }
-            options={[
-              { label: "Genera dal tipo", value: "generate" },
-              { label: "Importa da un'altra campagna", value: "import" },
-              { label: "Nessuna azione", value: "no-action" },
-            ]}
-            menuTargetQuery={"body"}
-            onChange={(option) => {
-              if (option === null || option.value === "no-action") {
-                setFieldValue("manualAction", "no-action");
-                setFieldValue("manualCampaign", undefined);
-                return;
-              }
-              setFieldValue("manualAction", option.value);
-            }}
-          />
-          {values?.manualAction === "import" && (
-            <div className="aq-mt-2">
-              <CampaignSelect
-                name="manualCampaign"
-                onBlur={() => {
-                  setFieldTouched("manualCampaign");
-                }}
-                label="Campagna da cui importare il manuale"
-                placeholder="Seleziona la campagna da cui importare il manuale"
-                onChange={(value) => {
-                  if (value === null || value.value === "") {
-                    setFieldValue("manualCampaign", undefined);
-                    return;
-                  }
-                  setFieldValue("manualCampaign", Number(value.value));
-                }}
-              />
-              {errors.manualCampaign && (
-                <ErrorMessageWrapper>
-                  {errors.manualCampaign}
-                </ErrorMessageWrapper>
-              )}
-            </div>
-          )}
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "16px",
+    <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+      <div>
+        <Select
+          name="previewAction"
+          onBlur={() => {
+            setFieldTouched("previewAction");
           }}
-        >
-          <Button
-            size="block"
-            flat
-            onClick={() => {
-              onClose();
-            }}
-          >
-            Annulla
-          </Button>
-          <Button
-            type="submit"
-            size="block"
-            disabled={isSubmitting}
-            onClick={() => {
-              validateForm().then((errors) => {
-                if (Object.keys(errors).length > 0) {
+          label="Preview"
+          value={
+            values?.previewAction
+              ? { label: "", value: values?.previewAction }
+              : { label: "Nessuna azione", value: "no-action" }
+          }
+          options={[
+            { label: "Genera dal tipo", value: "generate" },
+            { label: "Importa da un'altra campagna", value: "import" },
+            { label: "Nessuna azione", value: "no-action" },
+          ]}
+          menuTargetQuery={"body"}
+          onChange={(option) => {
+            if (option === null || option.value === "no-action") {
+              setFieldValue("previewAction", "no-action");
+              setFieldValue("previewCampaign", undefined);
+              return;
+            }
+
+            setFieldValue("previewAction", option.value);
+          }}
+        />
+        {values?.previewAction === "import" && (
+          <FormikField name="previewCampaign">
+            {({ field, form }: FieldProps) => (
+              <div className="aq-mt-2">
+                <CampaignSelect
+                  onBlur={() => {
+                    form.setFieldTouched("previewCampaign");
+                  }}
+                  name={field.name}
+                  label="Campagna da cui importare la preview"
+                  placeholder="Seleziona la campagna da cui importare la preview"
+                  onChange={(value) => {
+                    if (value === null || value.value === "") {
+                      form.setFieldValue("previewCampaign", undefined);
+                      return;
+                    }
+                    form.setFieldValue("previewCampaign", Number(value.value));
+                  }}
+                />
+                {errors.previewCampaign && (
+                  <ErrorMessageWrapper>
+                    {errors.previewCampaign}
+                  </ErrorMessageWrapper>
+                )}
+              </div>
+            )}
+          </FormikField>
+        )}
+      </div>
+      <div>
+        <Select
+          name="manualAction"
+          label="Manual"
+          onBlur={() => {
+            setFieldTouched("manualAction");
+          }}
+          value={
+            values?.manualAction
+              ? { label: "", value: values?.manualAction }
+              : { label: "Nessuna azione", value: "no-action" }
+          }
+          options={[
+            { label: "Genera dal tipo", value: "generate" },
+            { label: "Importa da un'altra campagna", value: "import" },
+            { label: "Nessuna azione", value: "no-action" },
+          ]}
+          menuTargetQuery={"body"}
+          onChange={(option) => {
+            if (option === null || option.value === "no-action") {
+              setFieldValue("manualAction", "no-action");
+              setFieldValue("manualCampaign", undefined);
+              return;
+            }
+            setFieldValue("manualAction", option.value);
+          }}
+        />
+        {values?.manualAction === "import" && (
+          <div className="aq-mt-2">
+            <CampaignSelect
+              name="manualCampaign"
+              onBlur={() => {
+                setFieldTouched("manualCampaign");
+              }}
+              label="Campagna da cui importare il manuale"
+              placeholder="Seleziona la campagna da cui importare il manuale"
+              onChange={(value) => {
+                if (value === null || value.value === "") {
+                  setFieldValue("manualCampaign", undefined);
                   return;
                 }
-                submitForm();
-              });
-            }}
-          >
-            Conferma
-          </Button>
-        </div>
+                setFieldValue("manualCampaign", Number(value.value));
+              }}
+            />
+            {errors.manualCampaign && (
+              <ErrorMessageWrapper>{errors.manualCampaign}</ErrorMessageWrapper>
+            )}
+          </div>
+        )}
       </div>
-    </Modal>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "16px",
+        }}
+      >
+        <Button
+          size="block"
+          flat
+          onClick={() => {
+            onClose();
+          }}
+        >
+          Annulla
+        </Button>
+        <Button
+          type="submit"
+          size="block"
+          disabled={isSubmitting}
+          onClick={() => {
+            validateForm().then((errors) => {
+              if (Object.keys(errors).length > 0) {
+                return;
+              }
+              submitForm();
+            });
+          }}
+        >
+          Conferma
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const Confirmation = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <div className="aq-mb-4">
+        Hai importato/generato le campagne correttamente
+      </div>
+      <Button size="block" onClick={onClose}>
+        Chiudi
+      </Button>
+    </div>
   );
 };
 
 const ImportPages = ({ dossier }: { dossier: FormProps["dossier"] }) => {
   const [open, setOpen] = useState(false);
+  const [confirmation, setConfirmation] = useState(false);
   const { values } = useFormikContext<NewCampaignValues>();
+  const [importManual] = usePostDossiersByCampaignManualMutation();
+  const [importPreview] = usePostDossiersByCampaignPreviewMutation();
 
-  if (!values.isEdit) return null;
+  if (!values.isEdit || !dossier) return null;
   return (
     <>
-      <Formik
+      <Formik<ImportPagesValues>
         initialValues={{
           previewAction: "no-action",
           previewCampaign: undefined,
@@ -222,14 +241,71 @@ const ImportPages = ({ dossier }: { dossier: FormProps["dossier"] }) => {
         validateOnBlur={false}
         onSubmit={async (values, actions) => {
           actions.setSubmitting(true);
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-          // alert(JSON.stringify(values));
-          setOpen(false);
+          if (values.previewAction === "import") {
+            await importPreview({
+              campaign: dossier.id.toString(),
+              body: {
+                importFrom: values.previewCampaign,
+              },
+            });
+          }
+          if (values.manualAction === "import") {
+            await importManual({
+              campaign: dossier.id.toString(),
+              body: {
+                importFrom: values.manualCampaign,
+              },
+            });
+          }
+
+          if (
+            values.previewAction === "generate" ||
+            values.manualAction === "generate"
+          ) {
+            await fetch(
+              `/wp-json/appq/v1/regenerate-campaign-pages/${dossier.id}`,
+              {
+                method: "POST",
+              }
+            );
+          }
+          if (
+            values.previewAction === "no-action" &&
+            values.manualAction === "no-action"
+          ) {
+            setOpen(false);
+          } else {
+            setConfirmation(true);
+          }
           actions.resetForm();
           actions.setSubmitting(false);
         }}
       >
-        <ImportPagesModal isOpen={open} onClose={() => setOpen(false)} />
+        <Modal
+          isOpen={open}
+          title="Import page"
+          onClose={() => {
+            setOpen(false);
+            setConfirmation(false);
+          }}
+          size="mid"
+        >
+          {confirmation ? (
+            <Confirmation
+              onClose={() => {
+                setOpen(false);
+                setConfirmation(false);
+              }}
+            />
+          ) : (
+            <ImportPagesModal
+              onClose={() => {
+                setOpen(false);
+                setConfirmation(false);
+              }}
+            />
+          )}
+        </Modal>
       </Formik>
       <Button size="block" onClick={() => setOpen(true)}>
         Open
