62f45301c1886701654d8ec52a5f63d81de1abab
Kariamos <minucci9292@gmail.com>
2025-06-11 16:42:25 +0200

    feat(GenderRequirements): update gender options and improve structure

--- src/pages/campaigns/components/campaignForm/fields/GenderRequirements.tsx
+++ src/pages/campaigns/components/campaignForm/fields/GenderRequirements.tsx
@@ -2,52 +2,68 @@ import {
   FormikField,
   FormLabel,
   Checkbox,
+  TextareaField,
 } from "@appquality/appquality-design-system";
-import { FieldProps } from "formik";
+import { FieldProps, useFormikContext } from "formik";
 import { NewCampaignValues } from "../FormProvider";
 import styled from "styled-components";
 
 const genderOptions = [
-  { value: "male", label: "Male" },
-  { value: "female", label: "Female" },
-  { value: "non-conforming", label: "Non conforming" },
-  { value: "prefer-not-to-answer", label: "Prefer not to answer" },
+  { value: "1", label: "Male" },
+  { value: "0", label: "Female" },
+  { value: "2", label: "Non conforming" },
+  { value: "-1", label: "Prefer not to answer" },
 ];
 
-const CheckboxWrapper = styled.div`
-  display: flex;
-  align-items: center;
+const CheckboxGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px 32px;
+  margin-bottom: 8px;
+  max-width: 420px;
 `;
 
-const GenderRequirements = () => (
-  <FormikField name="genderRequirements">
-    {({ field, form }: FieldProps<NewCampaignValues["genderRequirements"]>) => (
-      <CheckboxWrapper>
-        <FormLabel htmlFor={field.name} label={"Gender Requirements"} />
-        {genderOptions.map((option) => (
-          <Checkbox
-            key={option.value}
-            name={field.name}
-            label={option.label}
-            checked={field.value?.options.includes(option.value)}
-            onChange={(e) => {
-              if (e.target.checked) {
-                form.setFieldValue(field.name, [
-                  ...(field.value || []),
-                  option.value,
-                ]);
-              } else {
-                form.setFieldValue(
-                  field.name,
-                  field.value?.filter((item: string) => item !== option.value)
-                );
-              }
-            }}
-          />
-        ))}
-      </CheckboxWrapper>
-    )}
-  </FormikField>
-);
+const GenderRequirements = () => {
+  const { getFieldProps } = useFormikContext<NewCampaignValues>();
+  return (
+    <FormikField name="genderRequirements">
+      {({
+        field,
+        form,
+      }: FieldProps<NewCampaignValues["genderRequirements"]>) => {
+        const value = field.value || { customerChoice: "", options: [] };
+        return (
+          <div>
+            <FormLabel htmlFor={field.name} label={""} />
+            <CheckboxGrid>
+              {genderOptions.map((option) => (
+                <Checkbox
+                  key={option.value + option.label}
+                  name={`${field.name}.options`}
+                  label={option.label}
+                  checked={value.options.includes(option.value)}
+                  onChange={(e) => {
+                    const newOptions = e.target.checked
+                      ? [...value.options, option.value]
+                      : value.options.filter(
+                          (item: string) => item !== option.value
+                        );
+                    form.setFieldValue(`${field.name}.options`, newOptions);
+                  }}
+                />
+              ))}
+            </CheckboxGrid>
+            <TextareaField
+              {...getFieldProps(`${field.name}.customerChoice`)}
+              label="Customer requirements:"
+              resize="vertical"
+              placeholder="Specify any custom gender requirements here"
+            />
+          </div>
+        );
+      }}
+    </FormikField>
+  );
+};
 
 export default GenderRequirements;
