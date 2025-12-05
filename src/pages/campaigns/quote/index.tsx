a6da1c1855c038f3907695c6d84be7c9f23101f1
Kariamos <minucci9292@gmail.com>
2025-12-05 17:43:43 +0100

    refactor(QuotePage): extracted SummaryFinanceCard and FormSectionCard components

--- src/pages/campaigns/quote/index.tsx
+++ src/pages/campaigns/quote/index.tsx
@@ -13,6 +13,8 @@ import { useQuoteRecap } from "./useQuoteRecap";
 import { QuoteHistorySection } from "./sections/QuoteHistorySection";
 import { RevenueOverviewSection } from "./sections/RevenueOverviewSection";
 import { CostAndResourceDetailsSection } from "./sections/CostAndResourceDetailsSection";
+import { SummaryFinanceCard } from "./sections/SummaryFinanceCard";
+import { FormSectionCard } from "./sections/FormSectionCard";
 
 const FullGrid = styled(BSGrid)`
   width: 100%;
@@ -55,154 +57,8 @@ const EditCampaign = () => {
           <StickyContainer>
             <div className="aq-mb-4">
               <QuoteInput campaignId={id} />
-              <Card className="aq-mb-4" title="Summary finance">
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                  }}
-                >
-                  <div
-                    style={{
-                      color: aqBootstrapTheme.palette.primary,
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                    }}
-                  >
-                    <span style={{ fontWeight: "bold" }}>
-                      📥 TOTAL REVENUE:{" "}
-                    </span>
-                    <span
-                      style={{
-                        fontWeight: "bold",
-                        color: aqBootstrapTheme.palette.secondary,
-                      }}
-                    >
-                      --€
-                    </span>
-                  </div>
-                  <div
-                    style={{
-                      color: aqBootstrapTheme.palette.primary,
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                    }}
-                  >
-                    <span style={{ marginTop: "12px" }}>Community costs: </span>
-                    <span
-                      style={{
-                        color: aqBootstrapTheme.palette.primary,
-                      }}
-                    >
-                      --€
-                    </span>
-                  </div>
-                  <div
-                    style={{
-                      color: aqBootstrapTheme.palette.primary,
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                    }}
-                  >
-                    <span>Human Resource costs: </span>
-                    <span
-                      style={{
-                        color: aqBootstrapTheme.palette.primary,
-                      }}
-                    >
-                      --€
-                    </span>
-                  </div>
-                  <HorizontalDivider />
-                  <div
-                    style={{
-                      color: aqBootstrapTheme.palette.primary,
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                    }}
-                  >
-                    <span style={{ fontWeight: "bold" }}>📥 TOTAL COST: </span>
-                    <span
-                      style={{
-                        fontWeight: "bold",
-                        color: aqBootstrapTheme.palette.primary,
-                      }}
-                    >
-                      --€
-                    </span>
-                  </div>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      padding: "24px 24px 24px 30px",
-                      gap: "25px",
-                      backgroundColor: "#f9f9f9",
-                    }}
-                  >
-                    <BSCol size="col-9">
-                      <div
-                        style={{
-                          marginTop: "12px",
-                          display: "flex",
-                          alignItems: "center",
-                          alignContent: "center",
-                          gap: "4px 25px",
-                          flexDirection: "row",
-                          flexWrap: "wrap",
-                        }}
-                      >
-                        <span
-                          style={{
-                            fontWeight:
-                              aqBootstrapTheme.typography.fontWeight.bold,
-                          }}
-                        >
-                          💰 GROSS MARGIN
-                        </span>
-                        <strong
-                          style={{
-                            fontSize: "24px",
-                            fontWeight:
-                              aqBootstrapTheme.typography.fontWeight.bold,
-                            color: aqBootstrapTheme.colors.green,
-                          }}
-                        >
-                          --%
-                        </strong>
-                      </div>
-                    </BSCol>
-                  </div>
-                </div>
-              </Card>
-              <Card className="aq-mb-4" title="Form Section">
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                  }}
-                >
-                  <Steps className="aq-m-3" current={0} direction="vertical">
-                    <Steps.Step
-                      description=""
-                      isCompleted
-                      title="History activity quotation"
-                    />
-                    <Steps.Step description="" title="Revenue details" />
-                    <Steps.Step
-                      description=""
-                      title="Cost & Resource details"
-                    />
-                  </Steps>
-                </div>
-              </Card>
+              <SummaryFinanceCard />
+              <FormSectionCard />
             </div>
           </StickyContainer>
         </BSCol>
---
+++ src/pages/campaigns/quote/sections/SummaryFinanceCard.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import {
+  aqBootstrapTheme,
+  BSCol,
+  Card,
+} from "@appquality/appquality-design-system";
+import { styled } from "styled-components";
+
+const HorizontalDivider = styled.div`
+  margin-top: 12px;
+  margin-bottom: 12px;
+  width: 100%;
+  height: 1px;
+  background: #ccc;
+`;
+
+export const SummaryFinanceCard = () => {
+  return (
+    <Card className="aq-mb-4" title="Summary finance">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+        }}
+      >
+        <div
+          style={{
+            color: aqBootstrapTheme.palette.primary,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ fontWeight: "bold" }}>📥 TOTAL REVENUE: </span>
+          <span
+            style={{
+              fontWeight: "bold",
+              color: aqBootstrapTheme.palette.secondary,
+            }}
+          >
+            --€
+          </span>
+        </div>
+
+        <div
+          style={{
+            color: aqBootstrapTheme.palette.primary,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ marginTop: "12px" }}>Community costs: </span>
+          <span
+            style={{
+              color: aqBootstrapTheme.palette.primary,
+            }}
+          >
+            --€
+          </span>
+        </div>
+
+        <div
+          style={{
+            color: aqBootstrapTheme.palette.primary,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <span>Human Resource costs: </span>
+          <span
+            style={{
+              color: aqBootstrapTheme.palette.primary,
+            }}
+          >
+            --€
+          </span>
+        </div>
+
+        <HorizontalDivider />
+
+        <div
+          style={{
+            color: aqBootstrapTheme.palette.primary,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ fontWeight: "bold" }}>📥 TOTAL COST: </span>
+          <span
+            style={{
+              fontWeight: "bold",
+              color: aqBootstrapTheme.palette.primary,
+            }}
+          >
+            --€
+          </span>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "24px 24px 24px 30px",
+            gap: "25px",
+            backgroundColor: "#f9f9f9",
+          }}
+        >
+          <BSCol size="col-9">
+            <div
+              style={{
+                marginTop: "12px",
+                display: "flex",
+                alignItems: "center",
+                alignContent: "center",
+                gap: "4px 25px",
+                flexDirection: "row",
+                flexWrap: "wrap",
+              }}
+            >
+              <span
+                style={{
+                  fontWeight: aqBootstrapTheme.typography.fontWeight.bold,
+                }}
+              >
+                💰 GROSS MARGIN
+              </span>
+              <strong
+                style={{
+                  fontSize: "24px",
+                  fontWeight: aqBootstrapTheme.typography.fontWeight.bold,
+                  color: aqBootstrapTheme.colors.green,
+                }}
+              >
+                --%
+              </strong>
+            </div>
+          </BSCol>
+        </div>
+      </div>
+    </Card>
+  );
+};
