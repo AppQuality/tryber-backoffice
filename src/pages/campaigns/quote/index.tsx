f8e25eefe607ccae4b6b31dcaaec04663a935480
Kariamos <minucci9292@gmail.com>
2025-12-05 12:17:40 +0100

    feat(QuotePage): enhance quote history and revenue overview sections with new components and layout

--- src/pages/campaigns/quote/index.tsx
+++ src/pages/campaigns/quote/index.tsx
@@ -1,4 +1,13 @@
-import { BSCol, BSGrid, Card } from "@appquality/appquality-design-system";
+import {
+  Accordion,
+  aqBootstrapTheme,
+  BSCol,
+  BSGrid,
+  Button,
+  Card,
+  Dropdown,
+  Input,
+} from "@appquality/appquality-design-system";
 import { useParams } from "react-router-dom";
 import { PageTemplate } from "src/features/PageTemplate";
 import { styled } from "styled-components";
@@ -27,27 +36,88 @@ const EditCampaign = () => {
       <FullGrid>
         <BSCol size="col-lg-8">
           <Section
-            title="Activity Quotation"
-            subtitle="Set the price for this activity. Before proceeding, make sure to review the campaign dossier carefully and consider any previous quotes. If quotes have been created through app.unguess, they will be displayed directly on this page for easy comparison."
+            title="Quote History"
+            subtitle="Review past quotes for this campaign or compare with similar templates to ensure accurate pricing"
             id="quote"
           >
-            {data.history.length > 0 && (
-              <Card
-                className="aq-mb-4"
-                title="Previous quotes for this campaign"
+            <Card className="aq-mb-4" title="Quote history & comparison">
+              <Accordion initialActive={""} id="quote-history-accordion">
+                <Accordion.Item
+                  id="quote-history-campaign"
+                  title="Previous quotes for this campaign"
+                  key={"campaign-quotes"}
+                >
+                  {data.history.length > 0 && (
+                    <QuoteTable data={data.history} />
+                  )}
+                </Accordion.Item>
+                <Accordion.Item
+                  id="quote-history-template-workspace"
+                  title="All other quotes for this template in this workspace"
+                  key={"other-quotes"}
+                >
+                  {data.otherCampaigns.length > 0 ? (
+                    <QuoteTable data={data.otherCampaigns} />
+                  ) : (
+                    <div>This is the first quote for this workspace.</div>
+                  )}
+                </Accordion.Item>
+              </Accordion>
+            </Card>
+          </Section>
+          <Section
+            title="Revenue Overview"
+            subtitle="Track campaign revenue, token usage, and linked agreements"
+            id="quote"
+          >
+            <Card className="aq-mb-4" title="Revenue details">
+              <div
+                style={{
+                  display: "flex",
+                  gap: aqBootstrapTheme.grid.spacing.default,
+                }}
               >
-                <QuoteTable data={data.history} />
-              </Card>
-            )}
-            <Card
-              className="aq-mb-4"
-              title="All other quotes in this workspace"
-            >
-              {data.otherCampaigns.length > 0 ? (
-                <QuoteTable data={data.otherCampaigns} />
-              ) : (
-                <div>This is the first quote for this workspace.</div>
-              )}
+                <BSCol size="col-lg-2">
+                  <div>
+                    Token used{" "}
+                    <span style={{ color: aqBootstrapTheme.palette.danger }}>
+                      *
+                    </span>
+                  </div>
+                  <Input
+                    id="token-used"
+                    type="string"
+                    value=""
+                    placeholder="E.g. 10"
+                  />
+                </BSCol>
+                <BSCol size="col-lg-2">
+                  <div>
+                    Linked agreement{" "}
+                    <span style={{ color: aqBootstrapTheme.palette.danger }}>
+                      *
+                    </span>
+                  </div>
+                  <Dropdown
+                    name="agreement-dropdown"
+                    placeholder="Choose an agreement..."
+                    options={[
+                      { label: "Agreement 1", value: "1" },
+                      { label: "Agreement 2", value: "2" },
+                    ]}
+                  />
+                  <Button
+                    forwardedAs="a"
+                    href="https://tryber.me/backoffice/agreements/new"
+                    kind="link"
+                    target="_blank"
+                  >
+                    <span style={{ color: aqBootstrapTheme.palette.info }}>
+                      Agreement not found? Create a new one
+                    </span>
+                  </Button>
+                </BSCol>
+              </div>
             </Card>
           </Section>
         </BSCol>
